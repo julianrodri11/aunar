ff327bbe16905df3ce85fcab7cf4b5181a7d4474
Julian Rodriguez <julianrodri11@gmail.com>
2018-05-25 02:46:26 -0500

    Cambio del menu 2 por un menú que contiene sub opciones

--- menu2.php
+++ menu2.php
@@ -1,5 +1,5 @@
 <!-- ----------------MENÚ INSTITUCIONAL----------------------- -->  
-  <ul class="nav justify-content-center  p-4  f-negrita">
+  <!--ul class="nav justify-content-center  p-4  f-negrita">
     <li class="nav-item border-right borde-azul ">
       <a class="nav-link texto-azul hover-texto-amarillo" href="#">Institucional</a>
     </li>
@@ -21,5 +21,92 @@
     <li class="nav-item ">
       <a class="nav-link texto-azul hover-texto-amarillo" href="#">CIAN</a>
     </li>
+  </ul-->
+
+
+
+  <!-- Nav tabs -->
+  <ul class="nav nav-pills justify-content-center p-4" >
+    
+    <li class="nav-item   border-right borde-azul">
+      <a class="nav-link fuente-institucional texto-azul hover-texto-amarillo" data-toggle="pill" href="#institucional">Institucional</a>
+    </li>
+
+    
+    <li class="nav-item   border-right borde-azul">
+      <a class="nav-link fuente-institucional texto-azul hover-texto-amarillo" data-toggle="pill" href="#oferta-academica">Oferta Académica</a>
+    </li>
+
+    
+    <li class="nav-item   border-right borde-azul">
+      <a class="nav-link fuente-institucional texto-azul hover-texto-amarillo" data-toggle="pill" href="#investigacion">Investigación</a>
+    </li>
+
+    <li class="nav-item   border-right borde-azul">
+      <a class="nav-link fuente-institucional texto-azul hover-texto-amarillo" data-toggle="pill" href="#bienestar-universitario">Bienestar universitario</a>
+    </li>
+    
+    <li class="nav-item   border-right borde-azul">
+      <a class="nav-link fuente-institucional texto-azul hover-texto-amarillo" data-toggle="pill" href="#poryeccion-institucional">Proyección Institucional</a>
+    </li>
+
+    <li class="nav-item   border-right borde-azul">
+      <a class="nav-link fuente-institucional texto-azul hover-texto-amarillo" data-toggle="pill" href="#aunar-virtual">Aunar Virtual</a>
+    </li>
+
+    <li class="nav-item   borde-azul">
+      <a class="nav-link fuente-institucional texto-azul hover-texto-amarillo " data-toggle="pill" href="#cian">CIAN</a>
+    </li>
+
   </ul>
+
+
+
+  <!-- Tab panes -->
+  <div class="tab-content">
+
+    <div id="institucional" class="container bg-danger tab-pane fade"><br>
+      <h3>Institucional</h3><h4>menus y submenus - en desarrollo</h4>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua..</p>
+    </div>
+
+    <div id="oferta-academica" class="container bg-danger tab-pane fade"><br>
+      <h3>Oferta Académica</h3><h4>menus y submenus - en desarrollo</h4>
+      <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laudantium laborum dolorum, aliquid incidunt sapiente doloremque ullam fuga cum nihil facere.</p>
+    </div>
+
+    <div id="investigacion" class="container bg-danger tab-pane fade"><br>
+      <h3>investigación</h3><h4>menus y submenus - en desarrollo</h4>
+      <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, tota Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium, pariatur. m rem aperiam.</p>
+    </div>
+    
+    <div id="bienestar-universitario" class="container bg-danger tab-pane fade"><br>
+      <h3>Bienestar universitario</h3><h4>menus y submenus - en desarrollo</h4>
+      <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam veritatis dolorum amet consequatur rem quae nostrum id, harum error ducimus vero nam recusandae animi. Et iusto aspernatur error numquam quidem quibusdam perspiciatis iste ab blanditiis. accusantium doloremque laudantium, totam rem aperiam.</p>
+    </div>
+
+    <div id="poryeccion-institucional" class="container bg-danger tab-pane fade"><br>
+      <h3>Proyección Institucional</h3><h4>menus y submenus - en desarrollo</h4>
+      <p>Sed ut perspiciatis unde omnis iste natus error sit Lorem ipsum dolor sit amet, consectetur adipisicing elit. Mollitia ipsam dolores deserunt a laudantium amet unde sint, laboriosam. Nihil odio ab, architecto, deleniti cumque voluptatem quis temporibus quasi corporis, esse labore. Dolorum, nisi, reiciendis! Accusamus veniam, itaque eos voluptas unde deleniti commodi eligendi minima accusantium, magnam perferendis animi odio consectetur? voluptatem accusantium doloremque laudantium, totam rem aperiam.</p>
+    </div>
+
+    <div id="aunar-virtual" class="container bg-danger tab-pane fade"><br>
+      <h3>Aunar Virtual</h3><h4>menus y submenus - en desarrollo</h4>
+      <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam.</p>
+    </div>
+
+    <div id="cian" class="container bg-danger tab-pane fade"><br>
+      <h3>CIAN</h3><h4>menus y submenus - en desarrollo</h4>
+      <p>Sed ut perspiciatis unde omnis iste natus error sit Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iste fuga, a, molestiae unde blanditiis eos! Nobis adipisci excepturi, tempora illo obcaecati rem quo eius fuga error debitis! Tempore iste aspernatur deleniti maxime consectetur unde in, iusto vel cum ipsam, quisquam placeat dolorum. A sit rem debitis omnis, nam dolorem blanditiis eum ea magnam nostrum voluptate, mollitia, fugit officia excepturi magni. voluptatem accusantium doloremque laudantium, totam rem aperiam.</p>
+    </div>
+  </div>
+
+
+  <script>
+    $(document).ready(function(){
+      $(".nav-item a").click(function(){
+        $(this).tab('show');        
+      });
+    });
+  </script>
 <!-- ----------------FIN MENÚ INSTITUCIONAL------------------- -->
