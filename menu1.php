cda4df11bfa7b3606c01940ab85ec5b3ec7587c1
Julian Rodriguez <julianrodri11@gmail.com>
2018-05-27 14:29:13 -0500

    Creando archivo .htaccess url amigables

--- menu1.php
+++ menu1.php
@@ -1,6 +1,6 @@
 <!-------------------MENÚ PRINCIPAL-------------------------- -->
 <nav class="navbar navbar-expand-lg navbar-light fixed-top bg-white texto-azul " id="navbar1" >
-  <!--div class=" "><a class="mx-auto navbar-brand fuente-institucional texto-azul f-size-18 hover-amarillo" data-toggle="tooltip" data-placement="auto" title="Ir al inicio" href="index.php"><i class="fa fa-home"></i></a></div-->
+  <!--div class=" "><a class="mx-auto navbar-brand fuente-institucional texto-azul f-size-18 hover-amarillo" data-toggle="tooltip" data-placement="auto" title="Ir al inicio" href="index"><i class="fa fa-home"></i></a></div-->
   
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon "></span>
@@ -10,7 +10,7 @@
     
     <ul class="navbar-nav mr-auto ml-md-0 ml-sm-0 ml-0 ml-0  ">
       <li class="nav-item active pt-2 pb-2 hover-amarillo ">
-        <a class="nav-link texto-azul pl-md-2 pl-sm-2 border-left border-right borde-amarillo" href="inscripciones.php">Admisiones <span class="sr-only">(current)</span></a>
+        <a class="nav-link texto-azul pl-md-2 pl-sm-2 border-left border-right borde-amarillo" href="inscripciones">Admisiones <span class="sr-only">(current)</span></a>
       </li>
 
       <li class="nav-item pt-2 pb-2 dropdown hover-amarillo ">
@@ -33,7 +33,7 @@
       </li>
 
       <li class="nav-item pt-2 pb-2 hover-amarillo ">
-        <a class="nav-link texto-azul pl-md-2 pl-sm-2 border-right borde-amarillo" href="convocatorias.php">Convocatorias</a>
+        <a class="nav-link texto-azul pl-md-2 pl-sm-2 border-right borde-amarillo" href="convocatorias">Convocatorias</a>
       </li>
 
       <li class="nav-item pt-2 pb-2 dropdown hover-amarillo ">
