ec8be53bc844e5023aa2dfcd6575d6ba7bfe80b8
Julian Rodriguez <julianrodri11@gmail.com>
2018-05-28 15:51:23 -0500

    Correcciones en url y dialog inicial

--- index.php
+++ index.php
@@ -15,7 +15,7 @@
 <!-- --------------------MODAL INICIAL----------------------- -->
 <!-- The Modal -->
   <div class="modal fade" id="myModal">
-    <div class="modal-dialog modal-lg modal-dialog-centered">
+    <div class="modal-dialog modal-md modal-dialog-centered">
       <div class="modal-content">
       
         <!-- Modal Header -->
@@ -32,23 +32,50 @@
           <!-- Media middle -->
                <div class="card-group fuente-institucional texto-azul">
                 <div class="card">
-                  <img class="card-img-top" src="http://imgholder.ru/600x300/ccc/333.png?text=placeholder" alt="Card image cap">
+                  <img class="card-img-top" src="http://fpoimg.com/450x150?text=Preview&bg_color=fd8c8c&text_color=ffffff" alt="Card image cap">
                   <div class="card-body">
                     <h5 class="card-title f-negrita">Lorem ipsum.</h5>
                     <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laboriosam, eaque aliquam. Voluptate saepe, veniam unde!</p>
                     <p class="card-text"><small class="text-muted">Lorem ipsum. Last updated 3 mins ago</small></p>
                   </div>
                 </div>
-                <div class="card">
-                  <img class="card-img-top" src="http://imgholder.ru/600x300/ccc/333.png?text=placeholder" alt="Card image cap">
-                  <div class="card-body">
-                    <h5 class="card-title f-negrita">Lorem ipsum.</h5>
-                    <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quaerat expedita, sint ullam accusantium, magni consequatur.</p>
-                    <p class="card-text"><small class="text-muted">Lorem ipsum. Last updated 3 mins ago</small></p>
-                  </div>
-                </div>
-                
+               
               </div>
+
+ <!-- --------silider-------- -->
+              
+                <div id="myCarousel" class="carousel slide" data-ride="carousel">
+
+                  <!-- Indicators -->
+                  <ul class="carousel-indicators">
+                    <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+                    <li data-target="#myCarousel" data-slide-to="1"></li>
+                    <li data-target="#myCarousel" data-slide-to="2"></li>
+                  </ul>
+                  
+                  <!-- The slideshow -->
+                  <div class="carousel-inner">
+                    <div class="carousel-item active">
+                      <img class="img-fluid" src="http://dummyimage.com/400x150/5be332/333.png?text=AUTÓNOMA" alt="Los Angeles" width="1100" height="500">
+                    </div>
+                    <div class="carousel-item">
+                      <img class="img-fluid" src="http://imgholder.ru/400x150/6c35e3/333.png?text=placeholder" alt="Chicago" width="1100" height="500">
+                    </div>
+                    <div class="carousel-item">
+                      <img class="img-fluid" src="http://fpoimg.com/400x150?text=Preview&bg_color=fdb08c" alt="New York" width="1100" height="500">
+                    </div>
+                  </div>
+                  
+                  <!-- Left and right controls -->
+                  <a class="carousel-control-prev" href="#myCarousel" data-slide="prev">
+                    <span class="carousel-control-prev-icon"></span>
+                  </a>
+                  <a class="carousel-control-next" href="#myCarousel" data-slide="next">
+                    <span class="carousel-control-next-icon"></span>
+                  </a>
+                </div>
+              <!-- ----------------------- -->
+
           <!-- ----------- -->
 
         </div>
