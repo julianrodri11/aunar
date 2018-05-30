a009b6e1eec3a5dd1ddd32fa53a11a7ddad4fabc
Julian Rodriguez <julianrodri11@gmail.com>
2018-05-30 18:18:43 -0500

    Se coloca efecto hover sobre el logo - una linea top

--- logo-central.php
+++ logo-central.php
@@ -1,7 +1,7 @@
 <!----------------------- LOGO CENTRAL ----------------------- -->  
      
 <div class="d-flex justify-content-center  fixed-top " >
-  <div class="col-3 col-sm-2 col-md-2 col-lg-2 p-2 p-sm-3 p-md-1 pl-lg-4 pr-lg-4 pb-lg-4  mt-md-0 mt-sm-0 mt-2 position-absolute" id="cont-logo" data-toggle="tooltip" data-placement="bottom" title=".::AUTÓNOMA DE NARIÑO::.   Ir al inicio" >
+  <div class="col-3 col-sm-2 col-md-2 col-lg-2 p-2 p-sm-3 p-md-1 pl-lg-4 pr-lg-4 pb-lg-4  mt-md-0 mt-sm-0 mt-2 position-absolute hvr-overline-from-center" id="cont-logo" data-toggle="tooltip" data-placement="bottom" title=".::AUTÓNOMA DE NARIÑO::.   Ir al inicio" >
     <div class="p-1  p-sm-2  p-md-2 p-lg-2  rounded-circle" id="efecto-borde1">      
       <div class="p-1 p-sm-2 p-md-2 p-lg-2  rounded-circle" id="efecto-borde2" >      
          <a href="index.php"> <img class="rounded-circle  img-fluid rounded mx-auto d-block position-relative  "  alt="Logo Autónoma de Nariño" src="img/aunar.jpg"></a>
