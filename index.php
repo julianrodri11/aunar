fffd4de1235bea024850f32772683a7eaae3fe13
Julian Rodriguez <julianrodri11@gmail.com>
2018-05-31 02:08:58 -0500

    Se colocan imagenes de enlaces externos con sus respectivas url

--- index.php
+++ index.php
@@ -206,15 +206,78 @@
 <div class="container-fluid p-4  fondo-amarillo bg-white fuente-institucional">
   <div class="row text-center justify-content-center  ">    
     <div class=" col-12 fuente-institucional texto-azul f-negrita "><h1 class="pb-4 fuente-institucional texto-azul f-negrita">Enlaces Externos</h1></div>
-    <div class=" col-lg-2 col-md-3 col-sm-4 col-6 "><div class="p-4"><img class="img-fluid" src="img/icetex.png" alt="título imágen"></div><p class="pt-2 mb-0">Correo Aunar</p> </div>
-    <div class=" col-lg-2 col-md-3 col-sm-4 col-6 "><div class="p-4"><img class="img-fluid" src="img/icfes.png" alt="título imágen"></div><p class="pt-2 mb-0">Sistema Académia</p> </div>
-    <div class=" col-lg-2 col-md-3 col-sm-4 col-6 "><div class="p-4"><img class="img-fluid" src="img/icetex.png" alt="título imágen"></div><p class="pt-2 mb-0">Icfes</p> </div>
-    <div class=" col-lg-2 col-md-3 col-sm-4 col-6 "><div class="p-4"><img class="img-fluid" src="img/icfes.png" alt="título imágen"></div><p class="pt-2 mb-0">Eduteka</p> </div>
-    <div class=" col-lg-2 col-md-3 col-sm-4 col-6 "><div class="p-4"><img class="img-fluid" src="img/icetex.png" alt="título imágen"></div><p class="pt-2 mb-0">Dpto de Ciencia y Tecnología</p> </div>
-    <div class=" col-lg-2 col-md-3 col-sm-4 col-6 "><div class="p-4"><img class="img-fluid" src="img/icfes.png" alt="título imágen"></div><p class="pt-2 mb-0">Colombia Aprende</p> </div>
-    <div class=" col-lg-2 col-md-3 col-sm-4 col-6 "><div class="p-4"><img class="img-fluid" src="img/icetex.png" alt="título imágen"></div><p class="pt-2 mb-0">Banco Pichincha</p> </div>
-    <div class=" col-lg-2 col-md-3 col-sm-4 col-6 "><div class="p-4"><img class="img-fluid" src="img/icfes.png" alt="título imágen"></div><p class="pt-2 mb-0">Comuna</p> </div>
-    <div class=" col-lg-2 col-md-3 col-sm-4 col-6 "><div class="p-4"><img class="img-fluid" src="img/icetex.png" alt="título imágen"></div><p class="pt-2 mb-0">Otro enlace ...</p> </div>
+    <div class=" col-lg-2 col-md-3 col-sm-4 col-6 ">
+            <div class="p-4">
+              <a href="http://www.comuna.com.co/" class="hvr-wobble-vertical" target="_blanck">
+                <img class="img-fluid " src="img/comuna-cooperativa.png" alt="Comuna">
+              </a>
+            </div>
+            <p class="pt-2 mb-0 texto-azul">Comuna</p> 
+    </div>    
+    <div class=" col-lg-2 col-md-3 col-sm-4 col-6 ">
+            <div class="p-4">
+              <a href="https://www.bancopichincha.com.co/" class="hvr-wobble-vertical" target="_blanck">
+                <img class="img-fluid " src="img/pichincha.png" alt="Banco Pichincha Colombia">
+              </a>
+            </div>
+            <p class="pt-2 mb-0 texto-azul">Banco Pichincha</p> 
+    </div>        
+    <div class=" col-lg-2 col-md-3 col-sm-4 col-6 ">
+            <div class="p-4">
+              <a href="https://portal.icetex.gov.co/Portal/Home/HomeEstudiante/creditos-tu-eliges" class="hvr-wobble-vertical" target="_blanck">
+                <img class="img-fluid " src="img/icetex.png" alt="icetex">
+              </a>
+            </div>
+            <p class="pt-2 mb-0 texto-azul">Icetex</p> 
+    </div>
+    <div class=" col-lg-2 col-md-3 col-sm-4 col-6 ">
+            <div class="p-4">
+              <a href="http://www.icfesinteractivo.gov.co/" class="hvr-wobble-vertical" target="_blanck">
+                <img class="img-fluid " src="img/icfes.png" alt="Icfes">
+              </a>
+            </div>
+            <p class="pt-2 mb-0 texto-azul">Icfes</p> 
+    </div>
+    <div class=" col-lg-2 col-md-3 col-sm-4 col-6 ">
+            <div class="p-4">
+              <a href="https://www.mineducacion.gov.co" class="hvr-wobble-vertical" target="_blanck">
+                <img class="img-fluid " src="img/colciencias.jpg" alt="Colciencias">
+              </a>
+            </div>
+            <p class="pt-2 mb-0 texto-azul">Dpto de Ciencia y Tecnología</p> 
+    </div>
+    <div class=" col-lg-2 col-md-3 col-sm-4 col-6 ">
+            <div class="p-4">
+              <a href="http://aprende.colombiaaprende.edu.co" class="hvr-wobble-vertical" target="_blanck">
+                <img class="img-fluid " src="img/logo-colombiaprende.jpg" alt="Colombia aprende">
+              </a>
+            </div>
+            <p class="pt-2 mb-0 texto-azul">Colombia Aprende</p> 
+    </div>
+    <div class=" col-lg-2 col-md-3 col-sm-4 col-6 ">
+            <div class="p-4">
+              <a href="http://mail.google.com/a/aunar.edu.co/" class="hvr-wobble-vertical" target="_blanck">
+                <img class="img-fluid " src="http://dummyimage.com/600x300/378DB5/333.png?text=placeholder" alt="Correo institucional aunar">
+              </a>
+            </div>
+            <p class="pt-2 mb-0 texto-azul">Correo Aunar</p> 
+    </div>    
+    <div class=" col-lg-2 col-md-3 col-sm-4 col-6 ">
+            <div class="p-4">
+              <a href="http://www.eduteka.org/" class="hvr-wobble-vertical" target="_blanck">
+                <img class="img-fluid " src="img/Logo-Eduteka.png" alt="Eduteka">
+              </a>
+            </div>
+            <p class="pt-2 mb-0 texto-azul">Eduteka</p> 
+    </div>
+    <div class=" col-lg-2 col-md-3 col-sm-4 col-6 ">
+            <div class="p-4">
+              <a href="http://docentes.aunar.edu.co/" class="hvr-wobble-vertical" target="_blanck">
+                <img class="img-fluid " src="http://dummyimage.com/600x300/378DB5/333.png?text=placeholder" alt="Sistema de notas académia aunar">
+              </a>
+            </div>
+            <p class="pt-2 mb-0 texto-azul">Sistema Académia</p> 
+    </div>
     <!-- <div class=" col-lg-1 col-md-2 col-sm-4 col-6 "><div class="circulo p-3"><i class="fa fa-graduation-cap sombra-iconos">      </i></div><p class="pt-2 mb-0">Bisel Estudiantes</p> </div>
     <div class=" col-lg-1 col-md-2 col-sm-4 col-6 "><div class="circulo p-3"><i class="fa fa-leanpub sombra-iconos" >      </i></div><p class="pt-2 mb-0">Publicaciones</p> </div>       
     <div class=" col-lg-1 col-md-2 col-sm-4 col-6 "><div class="circulo p-3"><i class="fa fa-book sombra-iconos" >       </i></div><p class="pt-2 mb-0">Biblíoteca</p> </div>        -->
