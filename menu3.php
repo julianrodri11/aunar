989406554f0b91ee59cb39b3b9f49cf8f3be86ec
Julian Rodriguez <julianrodri11@gmail.com>
2018-06-05 01:13:16 -0500

    Animación en menú 3

--- menu3.php
+++ menu3.php
@@ -7,8 +7,8 @@
   <div class="row text-center justify-content-center  fuente-institucional f-x-small">    
     <div class=" col-xl-1 col-lg-2 col-md-2  col-sm-2 col-4 ">
         <div class="p-2 ">
-            <a href="#" data-toggle="tooltip" data-placement="top" title="Ir a " target="_blanck">
-                <img class="img-fluid " src="img/iconos/periodico.svg" alt="">
+            <a href="#" data-toggle="tooltip" data-placement="bottom" title="Ir a " target="_blanck">
+                <img class="img-fluid hvr-pop" src="img/iconos/periodico.svg" alt="">
             </a>
         </div>
             <p class="pt-2 mb-0 f-size-16">Periódico Estudiantil</p> 
@@ -17,8 +17,8 @@
 
     <div class=" col-xl-1 col-lg-2 col-md-2  col-sm-2 col-4 ">
         <div class="p-2 ">
-            <a href="#" data-toggle="tooltip" data-placement="top" title="Ir a " target="_blanck">
-                <img class="img-fluid " src="img/iconos/album.svg" alt="">
+            <a href="#" data-toggle="tooltip" data-placement="bottom" title="Ir a " target="_blanck">
+                <img class="img-fluid hvr-pop" src="img/iconos/album.svg" alt="">
             </a>
         </div>
             <p class="pt-2 mb-0 f-size-16">Albúm Fotográfico</p> 
@@ -27,8 +27,8 @@
 
     <div class=" col-xl-1 col-lg-2 col-md-2  col-sm-2 col-4 ">
         <div class="p-2 ">
-            <a href="https://www.youtube.com/watch?v=sQVbUfhm2ZQ" data-toggle="tooltip" data-placement="top" title="Ver Video institucional " target="_blanck">
-                <img class="img-fluid " src="img/iconos/v-institucional.svg" alt="">
+            <a href="https://www.youtube.com/watch?v=sQVbUfhm2ZQ" data-toggle="tooltip" data-placement="bottom" title="Ver Video institucional " target="_blanck">
+                <img class="img-fluid hvr-pop" src="img/iconos/v-institucional.svg" alt="">
             </a>
         </div>
             <p class="pt-2 mb-0 f-size-16">Video Institucional</p> 
@@ -37,8 +37,8 @@
 
     <div class=" col-xl-1 col-lg-2 col-md-2  col-sm-2 col-4 ">
         <div class="p-2 ">
-            <a href="#" data-toggle="tooltip" data-placement="top" title="Ir a " target="_blanck">
-                <img class="img-fluid " src="img/iconos/astereo.svg" alt="">
+            <a href="#" data-toggle="tooltip" data-placement="bottom" title="Ir a " target="_blanck">
+                <img class="img-fluid hvr-pop" src="img/iconos/astereo.svg" alt="">
             </a>
         </div>
             <p class="pt-2 mb-0 f-size-16">Aunar Stereo</p> 
@@ -47,8 +47,8 @@
 
     <div class=" col-xl-1 col-lg-2 col-md-2  col-sm-2 col-4 ">
         <div class="p-2 ">
-            <a href="https://www.youtube.com/watch?v=rNiBnmYdKtQ" data-toggle="tooltip" data-placement="top" title="Ver video Aunar TV " target="_blanck">
-                <img class="img-fluid " src="img/iconos/aunartv.svg" alt="">
+            <a href="https://www.youtube.com/watch?v=rNiBnmYdKtQ" data-toggle="tooltip" data-placement="bottom" title="Ver video Aunar TV " target="_blanck">
+                <img class="img-fluid hvr-pop" src="img/iconos/aunartv.svg" alt="">
             </a>
         </div>
             <p class="pt-2 mb-0 f-size-16">Aunar TV</p> 
@@ -57,8 +57,8 @@
 
     <div class=" col-xl-1 col-lg-2 col-md-2  col-sm-2 col-4 ">
         <div class="p-2 ">
-            <a href="http://campus.aunar.edu.co/" data-toggle="tooltip" data-placement="top" title="Ir a Campus Virtual " target="_blanck">
-                <img class="img-fluid " src="img/iconos/campus-virtual.svg" alt="">
+            <a href="http://campus.aunar.edu.co/" data-toggle="tooltip" data-placement="bottom" title="Ir a Campus Virtual " target="_blanck">
+                <img class="img-fluid hvr-pop" src="img/iconos/campus-virtual.svg" alt="">
             </a>
         </div>
             <p class="pt-2 mb-0 f-size-16">Campus Virtual</p> 
@@ -67,8 +67,8 @@
 
     <div class=" col-xl-1 col-lg-2 col-md-2  col-sm-2 col-4 ">
         <div class="p-2 ">
-            <a href="http://aunartech.aunar.edu.co/" data-toggle="tooltip" data-placement="top" title="Ir a Aunar Tech" target="_blanck">
-                <img class="img-fluid " src="img/iconos/atech.svg" alt="">
+            <a href="http://aunartech.aunar.edu.co/" data-toggle="tooltip" data-placement="bottom" title="Ir a Aunar Tech" target="_blanck">
+                <img class="img-fluid hvr-pop" src="img/iconos/atech.svg" alt="">
             </a>
         </div>
             <p class="pt-2 mb-0 f-size-16">Aunar Tech</p> 
@@ -77,8 +77,8 @@
 
     <div class=" col-xl-1 col-lg-2 col-md-2  col-sm-2 col-4 ">
         <div class="p-2 ">
-            <a href="http://cursos.aunar.edu.co/" data-toggle="tooltip" data-placement="top" title="Ir a Campus Presencial" target="_blanck">
-                <img class="img-fluid " src="img/iconos/campus-presencial.svg" alt="">
+            <a href="http://cursos.aunar.edu.co/" data-toggle="tooltip" data-placement="bottom" title="Ir a Campus Presencial" target="_blanck">
+                <img class="img-fluid hvr-pop" src="img/iconos/campus-presencial.svg" alt="">
             </a>
         </div>
             <p class="pt-2 mb-0 f-size-16">Campus Presencial</p> 
@@ -87,8 +87,8 @@
 
     <div class=" col-xl-1 col-lg-2 col-md-2  col-sm-2 col-4 ">
         <div class="p-2 ">
-            <a href="http://docentes.aunar.edu.co/" data-toggle="tooltip" data-placement="top" title="Ir a Bisel Docentes" target="_blanck">
-                <img class="img-fluid " src="img/iconos/bdocentes.svg" alt="">
+            <a href="http://docentes.aunar.edu.co/" data-toggle="tooltip" data-placement="bottom" title="Ir a Bisel Docentes" target="_blanck">
+                <img class="img-fluid hvr-pop" src="img/iconos/bdocentes.svg" alt="">
             </a>
         </div>
             <p class="pt-2 mb-0 f-size-16">Bisel Docentes</p>
@@ -97,8 +97,8 @@
 
     <div class=" col-xl-1 col-lg-2 col-md-2  col-sm-2 col-4 ">
         <div class="p-2 ">
-            <a href="http://estudiantes.aunar.edu.co/" data-toggle="tooltip" data-placement="top" title="Ir a Bisel Estudiantes" target="_blanck">
-                <img class="img-fluid " src="img/iconos/bestudiantes.svg" alt="">
+            <a href="http://estudiantes.aunar.edu.co/" data-toggle="tooltip" data-placement="bottom" title="Ir a Bisel Estudiantes" target="_blanck">
+                <img class="img-fluid hvr-pop" src="img/iconos/bestudiantes.svg" alt="">
             </a>
         </div>
             <p class="pt-2 mb-0 f-size-16">Bisel Estudiantes</p> 
@@ -107,8 +107,8 @@
 
     <div class=" col-xl-1 col-lg-2 col-md-2  col-sm-2 col-4 ">
         <div class="p-2 ">
-            <a href="#" data-toggle="tooltip" data-placement="top" title="Ir a " target="_blanck">
-                <img class="img-fluid " src="img/iconos/publicaciones.svg" alt="">
+            <a href="#" data-toggle="tooltip" data-placement="bottom" title="Ir a " target="_blanck">
+                <img class="img-fluid hvr-pop" src="img/iconos/publicaciones.svg" alt="">
             </a>
         </div>
             <p class="pt-2 mb-0 f-size-16">Publicaciones</p> </div> 
@@ -117,8 +117,8 @@
 
     <div class=" col-xl-1 col-lg-2 col-md-2  col-sm-2 col-4 ">
         <div class="p-2 ">
-            <a href="http://biblioteca.aunar.edu.co/" data-toggle="tooltip" data-placement="top" title="Ir a Biblíoteca" target="_blanck">
-                <img class="img-fluid " src="img/iconos/biblioteca.svg" alt="">
+            <a href="http://biblioteca.aunar.edu.co/" data-toggle="tooltip" data-placement="bottom" title="Ir a Biblíoteca" target="_blanck">
+                <img class="img-fluid hvr-pop" src="img/iconos/biblioteca.svg" alt="">
             </a>
         </div>
             <p class="pt-2 mb-0 f-size-16">Biblíoteca</p> </div> 
