0768690c298fe4b6635315783135ab2929321cc3
Julian Rodriguez <julianrodri11@gmail.com>
2018-06-17 00:56:08 -0500

    Slider de noticias

--- noticias/noticia-1.php
+++ noticias/noticia-1.php
@@ -74,119 +74,159 @@
   <!-- fin noticia 2 -->
 </div>
 
-  <div class="container bg-primary">
-    <div class="row d-flex justify-content-center">
-      <div class="col-12">
+  <div class="container ">
+    <div class="row d-flex justify-content-center align-items-center">
+      <div class="col-12 ">
       <!-- silider -->
               
-                <div id="myCarousel" class="carousel slide" data-ride="carousel" data-interval="500">
+              <div id="myCarousel2" class="carousel slide " data-ride="carousel" data-interval="2000">
 
-                  <!-- Indicators -->
-                  <ul class="carousel-indicators">
-                    <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
-                    <li data-target="#myCarousel" data-slide-to="1"></li>
-                    <li data-target="#myCarousel" data-slide-to="2"></li>
-                  </ul>
-                  
-                  <!-- The slideshow -->
-                  <div class="carousel-inner">
-                    <div class="carousel-item active bg-success">
-                      <div class="card-group">
-                          <div class="card p-4">
-                            <img class="card-img-top" src="http://dummyimage.com/150x100/3f73f5/333.png?text=NOTICIAS" alt="Card image cap" width="90%">
-                            <div class="card-body fuente-institucional texto-azul">
+              <!-- Indicators -->
+              <!-- <ul class="carousel-indicators ">
+                <li data-target="#myCarousel" class="fondo-azul-oscuro" data-slide-to="0" class="active"></li>
+                <li data-target="#myCarousel" class="fondo-azul-oscuro" data-slide-to="1"></li>
+                <li data-target="#myCarousel" class="fondo-azul-oscuro" data-slide-to="2"></li>
+              </ul> -->
+              
+              <!-- The slideshow -->
+              <div class="carousel-inner ">
+                <div class="carousel-item p-5 active">
+                  <div class="row p-2 m-5 ">
+                    <div class="col-12 col-md-6 col-lg-4">  
+                        <!-- noticia -->
+                          <div class="card mb-3 fuente-institucional">
+                            <img class="card-img-top img-fluid" src="img/noticias/noticia1.png" alt="Card image cap" width="100%">
+                            <div class="card-body">
                               <h5 class="card-title f-negrita">MAQUINARTE</h5>
-                              <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eius cupiditate labore ratione fugiat culpa, quidem eveniet accusantium itaque adipisci ad?</p>
-                              <p class="card-text"><small class="text-muted">Leer más</small></p>
+                              <p class="card-text f-myriad text-justify">La sub Red de facultades de Ingenierías de Nariño, invita a todos los investigadores de #AunarIngenierías para que participen de este importante eventoMAQUINARTE: IX MUESTRA DE PROYECTOS DE INGENIERíA</p>
+                              <a href="#" class="p-2 border borde-azul badge badge-pill hover-amarillo hover-texto-blanco texto-azul">leer más</a>
                             </div>
                           </div>
-                          <div class="card p-4">
-                            <img class="card-img-top" src="http://dummyimage.com/150x100/3f73f5/333.png?text=NOTICIAS" alt="Card image cap" width="90%">
-                            <div class="card-body fuente-institucional texto-azul">
-                              <h5 class="card-title f-negrita">MAQUINARTE</h5>
-                              <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis nisi animi voluptatibus omnis aliquam cumque sint consequatur expedita similique et.</p>
-                              <p class="card-text"><small class="text-muted">Leer más</small></p>
-                            </div>
-                          </div>
-                          <div class="card p-4">
-                            <img class="card-img-top" src="http://dummyimage.com/150x100/3f73f5/333.png?text=NOTICIAS" alt="Card image cap" width="90%">
-                            <div class="card-body fuente-institucional texto-azul">
-                              <h5 class="card-title f-negrita">MAQUINARTE</h5>
-                              <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Harum, unde, placeat. Totam consequatur consectetur ipsam provident laborum, velit maiores obcaecati.</p>
-                              <p class="card-text"><small class="text-muted">Leer más</small></p>
-                            </div>
-                          </div>
-                        </div>
+                        <!-- fin noticia -->
                     </div>
-                    <div class="carousel-item p-4 bg-warning">
-                      <div class="card-group">
-                          <div class="card p-4">
-                            <img class="card-img-top" src="http://dummyimage.com/150x100/AFAECE/333.png?text=NOTICIAS" alt="Card image cap" width="90%">
-                            <div class="card-body fuente-institucional texto-azul">
+                    <div class="col-12 col-md-6 col-lg-4">  
+                        <!-- noticia -->
+                          <div class="card mb-3 fuente-institucional">
+                            <img class="card-img-top img-fluid" src="img/noticias/noticia2.png" alt="Card image cap" width="100%">
+                            <div class="card-body">
                               <h5 class="card-title f-negrita">MAQUINARTE</h5>
-                              <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eius cupiditate labore ratione fugiat culpa, quidem eveniet accusantium itaque adipisci ad?</p>
-                              <p class="card-text"><small class="text-muted">Leer más</small></p>
+                              <p class="card-text f-myriad text-justify">La sub Red de facultades de Ingenierías de Nariño, invita a todos los investigadores de #AunarIngenierías para que participen de este importante eventoMAQUINARTE: IX MUESTRA DE PROYECTOS DE INGENIERíA</p>
+                              <a href="#" class="p-2 border borde-azul badge badge-pill hover-amarillo hover-texto-blanco texto-azul">leer más</a>
                             </div>
                           </div>
-                          <div class="card p-4">
-                            <img class="card-img-top" src="http://dummyimage.com/150x100/AFAECE/333.png?text=NOTICIAS" alt="Card image cap" width="90%">
-                            <div class="card-body fuente-institucional texto-azul">
-                              <h5 class="card-title f-negrita">MAQUINARTE</h5>
-                              <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis nisi animi voluptatibus omnis aliquam cumque sint consequatur expedita similique et.</p>
-                              <p class="card-text"><small class="text-muted">Leer más</small></p>
-                            </div>
-                          </div>
-                          <div class="card p-4">
-                            <img class="card-img-top" src="http://dummyimage.com/150x100/AFAECE/333.png?text=NOTICIAS" alt="Card image cap" width="90%">
-                            <div class="card-body fuente-institucional texto-azul">
-                              <h5 class="card-title f-negrita">MAQUINARTE</h5>
-                              <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Harum, unde, placeat. Totam consequatur consectetur ipsam provident laborum, velit maiores obcaecati.</p>
-                              <p class="card-text"><small class="text-muted">Leer más</small></p>
-                            </div>
-                          </div>
-                        </div>
+                        <!-- fin noticia -->
                     </div>
-                    <div class="carousel-item p-4 bg-success">
-                      <div class="card-group">
-                          <div class="card p-4">
-                            <img class="card-img-top" src="http://dummyimage.com/150x100/898989/333.png?text=NOTICIAS" alt="Card image cap" width="90%">
-                            <div class="card-body fuente-institucional texto-azul">
+                    <div class="col-12 col-md-6 col-lg-4">  
+                        <!-- noticia -->
+                          <div class="card mb-3 fuente-institucional">
+                            <img class="card-img-top img-fluid" src="img/noticias/noticia3.png" alt="Card image cap" width="100%">
+                            <div class="card-body">
                               <h5 class="card-title f-negrita">MAQUINARTE</h5>
-                              <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eius cupiditate labore ratione fugiat culpa, quidem eveniet accusantium itaque adipisci ad?</p>
-                              <p class="card-text"><small class="text-muted">Leer más</small></p>
+                              <p class="card-text f-myriad text-justify">La sub Red de facultades de Ingenierías de Nariño, invita a todos los investigadores de #AunarIngenierías para que participen de este importante eventoMAQUINARTE: IX MUESTRA DE PROYECTOS DE INGENIERíA</p>
+                              <a href="#" class="p-2 border borde-azul badge badge-pill hover-amarillo hover-texto-blanco texto-azul">leer más</a>
                             </div>
                           </div>
-                          <div class="card p-4">
-                            <img class="card-img-top" src="http://dummyimage.com/150x100/898989/333.png?text=NOTICIAS" alt="Card image cap" width="90%">
-                            <div class="card-body fuente-institucional texto-azul">
-                              <h5 class="card-title f-negrita">MAQUINARTE</h5>
-                              <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis nisi animi voluptatibus omnis aliquam cumque sint consequatur expedita similique et.</p>
-                              <p class="card-text"><small class="text-muted">Leer más</small></p>
-                            </div>
-                          </div>
-                          <div class="card p-4">
-                            <img class="card-img-top" src="http://dummyimage.com/150x100/898989/333.png?text=NOTICIAS" alt="Card image cap" width="90%">
-                            <div class="card-body fuente-institucional texto-azul">
-                              <h5 class="card-title f-negrita">MAQUINARTE</h5>
-                              <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Harum, unde, placeat. Totam consequatur consectetur ipsam provident laborum, velit maiores obcaecati.</p>
-                              <p class="card-text"><small class="text-muted">Leer más</small></p>
-                            </div>
-                          </div>
-                        </div>
+                        <!-- fin noticia -->
                     </div>
-                  </div> <!--fin carrousel inner -->
-                  
-                  <!-- Left and right controls -->
-                  <a class="carousel-control-prev" href="#myCarousel" data-slide="prev">
-                    <span class="carousel-control-prev-icon"></span>
-                  </a>
-                  <a class="carousel-control-next" href="#myCarousel" data-slide="next">
-                    <span class="carousel-control-next-icon"></span>
-                  </a>
+                  </div>
+                </div>
+
+                <div class="carousel-item p-5 ">
+                  <div class="row p-2 m-5 ">
+                    <div class="col-12 col-md-6 col-lg-4">  
+                        <!-- noticia -->
+                          <div class="card mb-3 fuente-institucional">
+                            <img class="card-img-top img-fluid" src="img/noticias/noticia1.png" alt="Card image cap" width="100%">
+                            <div class="card-body">
+                              <h5 class="card-title f-negrita">MAQUINARTE</h5>
+                              <p class="card-text f-myriad text-justify">La sub Red de facultades de Ingenierías de Nariño, invita a todos los investigadores de #AunarIngenierías para que participen de este importante eventoMAQUINARTE: IX MUESTRA DE PROYECTOS DE INGENIERíA</p>
+                              <a href="#" class="p-2 border borde-azul badge badge-pill hover-amarillo hover-texto-blanco texto-azul">leer más</a>
+                            </div>
+                          </div>
+                        <!-- fin noticia -->
+                    </div>
+                    <div class="col-12 col-md-6 col-lg-4">  
+                        <!-- noticia -->
+                          <div class="card mb-3 fuente-institucional">
+                            <img class="card-img-top img-fluid" src="img/noticias/noticia2.png" alt="Card image cap" width="100%">
+                            <div class="card-body">
+                              <h5 class="card-title f-negrita">MAQUINARTE</h5>
+                              <p class="card-text f-myriad text-justify">La sub Red de facultades de Ingenierías de Nariño, invita a todos los investigadores de #AunarIngenierías para que participen de este importante eventoMAQUINARTE: IX MUESTRA DE PROYECTOS DE INGENIERíA</p>
+                              <a href="#" class="p-2 border borde-azul badge badge-pill hover-amarillo hover-texto-blanco texto-azul">leer más</a>
+                            </div>
+                          </div>
+                        <!-- fin noticia -->
+                    </div>
+                    <div class="col-12 col-md-6 col-lg-4">  
+                        <!-- noticia -->
+                          <div class="card mb-3 fuente-institucional">
+                            <img class="card-img-top img-fluid" src="img/noticias/noticia3.png" alt="Card image cap" width="100%">
+                            <div class="card-body">
+                              <h5 class="card-title f-negrita">MAQUINARTE</h5>
+                              <p class="card-text f-myriad text-justify">La sub Red de facultades de Ingenierías de Nariño, invita a todos los investigadores de #AunarIngenierías para que participen de este importante eventoMAQUINARTE: IX MUESTRA DE PROYECTOS DE INGENIERíA</p>
+                              <a href="#" class="p-2 border borde-azul badge badge-pill hover-amarillo hover-texto-blanco texto-azul">leer más</a>
+                            </div>
+                          </div>
+                        <!-- fin noticia -->
+                    </div>
+                  </div>
+                </div>
+
+                <div class="carousel-item p-5 ">
+                  <div class="row p-2 m-5 ">
+                    <div class="col-12 col-md-6 col-lg-4">  
+                        <!-- noticia -->
+                          <div class="card mb-3 fuente-institucional">
+                            <img class="card-img-top img-fluid" src="img/noticias/noticia1.png" alt="Card image cap" width="100%">
+                            <div class="card-body">
+                              <h5 class="card-title f-negrita">MAQUINARTE</h5>
+                              <p class="card-text f-myriad text-justify">La sub Red de facultades de Ingenierías de Nariño, invita a todos los investigadores de #AunarIngenierías para que participen de este importante eventoMAQUINARTE: IX MUESTRA DE PROYECTOS DE INGENIERíA</p>
+                              <a href="#" class="p-2 border borde-azul badge badge-pill hover-amarillo hover-texto-blanco texto-azul">leer más</a>
+                            </div>
+                          </div>
+                        <!-- fin noticia -->
+                    </div>
+                    <div class="col-12 col-md-6 col-lg-4">  
+                        <!-- noticia -->
+                          <div class="card mb-3 fuente-institucional">
+                            <img class="card-img-top img-fluid" src="img/noticias/noticia2.png" alt="Card image cap" width="100%">
+                            <div class="card-body">
+                              <h5 class="card-title f-negrita">MAQUINARTE</h5>
+                              <p class="card-text f-myriad text-justify">La sub Red de facultades de Ingenierías de Nariño, invita a todos los investigadores de #AunarIngenierías para que participen de este importante eventoMAQUINARTE: IX MUESTRA DE PROYECTOS DE INGENIERíA</p>
+                              <a href="#" class="p-2 border borde-azul badge badge-pill hover-amarillo hover-texto-blanco texto-azul">leer más</a>
+                            </div>
+                          </div>
+                        <!-- fin noticia -->
+                    </div>
+                    <div class="col-12 col-md-6 col-lg-4">  
+                        <!-- noticia -->
+                          <div class="card mb-3 fuente-institucional">
+                            <img class="card-img-top img-fluid" src="img/noticias/noticia3.png" alt="Card image cap" width="100%">
+                            <div class="card-body">
+                              <h5 class="card-title f-negrita">MAQUINARTE</h5>
+                              <p class="card-text f-myriad text-justify">La sub Red de facultades de Ingenierías de Nariño, invita a todos los investigadores de #AunarIngenierías para que participen de este importante eventoMAQUINARTE: IX MUESTRA DE PROYECTOS DE INGENIERíA</p>
+                              <a href="#" class="p-2 border borde-azul badge badge-pill hover-amarillo hover-texto-blanco texto-azul">leer más</a>
+                            </div>
+                          </div>
+                        <!-- fin noticia -->
+                    </div>
+                  </div>
                 </div>
                 
-                <!-- fin slider -->
-                </div>
+              </div>
+              
+              <!-- Left and right controls -->
+              <a class="carousel-control-prev" href="#myCarousel2" data-slide="prev">
+                <span class="carousel-control-prev-icon fondo-azul-oscuro"></span>
+              </a>
+              <a class="carousel-control-next" href="#myCarousel2" data-slide="next">
+                <span class="carousel-control-next-icon fondo-azul-oscuro"></span>
+              </a>
+            </div>
+                
+                
+      <!-- fin slider -->
+      </div>
     </div>
   </div>
 
