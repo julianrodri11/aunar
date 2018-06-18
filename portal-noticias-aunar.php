9b4d9dc3603d4ce33ba5bb5bd5ca3323cfb84a59
Julian Rodriguez <julianrodri11@gmail.com>
2018-06-18 09:34:03 -0500

    noticias

--- portal-noticias-aunar.php
+++ portal-noticias-aunar.php
@@ -42,10 +42,25 @@
 				</div>
 				<div class="col-10 p-lg-5 fondo-gris-c">
 					<div class="row d-flex justify-content-center fuente-institucional">
-						<div class="col-8 text-center f-negrita texto-azul border-bottom borde-azul pb-3">
+						
+						<div class="col-8 text-center f-negrita texto-azul border-bottom borde-azul pb-3 f-size-18">
 							Ipialeña ha conseguido importantes reconocimientos en campeonatos internacionales de ciclo montañismo
 						</div>
-						<div class="col-8"></div>
+
+						<div class="col-8 pt-sans f-italica p-lg-4"><p>Leidy Mera, estudiante de la Corporación Universitaria Autónoma de Nariño, sede Ipiales, ha ganado importantes reconocimientos a nivel mundial debido a su pasión y dedicación por el ciclo montañismo ¡Conozcamos esta desafiante historia!</p></div>
+						
+						<div class="col-10">
+							<img class="mx-auto img-fluid" width="100%" src="img/noticias/slider-portal-de-noticias.jpg"/>
+						</div>
+						<div class="col-10 text-justify ">
+							<h5 class="text-center">¿Quién eres?</h5>
+							<p>Mi nombre es Leidy Johana Mera, pertenezco a la Corporación Universitaria Autónoma de Nariño y hace diez años practico el ciclo montañismo.</p>
+							<p>Cuéntanos tu historia con este deporte</p>
+							<p>Me llamó la atención practicar este deporte por mi hermano, ya que lo acompañaba a las competencias. Me llamaba mucho la atención conocer este deporte y todas las cosas nuevas que uno aprende a diario. Todo lo que uno viaja y conoce lugares, y las experiencias que uno tiene.</p>
+							<p>¿Qué es lo que más te gusta del ciclo montañismo?</p>
+							<p>Lo que más me gusta del ciclo montañismo es que uno se siente muy libre en esto, recorriendo todos los lugares, paisajes, y sobretodo que uno conoce a diario muchos lugares. También cuando uno sale del país y conoce diferentes costumbres, cosas y lo más bonito de todo es que se aprende y son enseñanzas para toda la vida.</p>
+						</div>
+
 					</div>
 				</div>
 			</div>
