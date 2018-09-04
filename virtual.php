6667a23760f3a4559da810d8c632a81d9a4082c7
Julian Rodriguez <julianrodri11@gmail.com>
2018-09-04 13:00:38 -0500

    Efecto overlay sobre imagenes de galira de aunar virtual

--- virtual.php
+++ virtual.php
@@ -24,7 +24,6 @@
 		</div>
 		
 		<?php include_once('secciones/menu2.php');?>
-
 		<?php include_once('secciones/menu_aunar_virtual.php');?>
 		
 		<!-- ----------------SECCIÓN NOTICIAS------------------------- -->
@@ -59,11 +58,11 @@
 						<div class="tab-pane fade p-3 fondo-gris-os" id="v-pills-profile" role="tabpanel" aria-labelledby="v-pills-profile-tab">
 							<div class="col-12 p-2 px-md-5 text-justify texto-azul-os">
 								
-								<strong> 1.	¿Cómo ingresar al Campus Aunar Virtual?</strong>								
+								<strong> 1.	¿Cómo ingresar al Campus Aunar Virtual?</strong>
 								<p>Lo primero que se debe hacer para ingresar a la Plataforma Moodle es contar con acceso a Internet, seleccionar el navegador: Firefox Mozilla o Google Chrome e ingresar a la siguiente dirección: www.aunar.edu.co</p>
 								<p>En la parte derecha del menú de navegación horizontal, se deberá seleccionar el enlace Aunar Virtual para ingresar al Portal Virtual, donde encontrará la siguiente opción de ingreso al Campus Aunar Virtual:</p>
 								
-								<p>También puede ingresar digitando la dirección Web http://campus.aunar.edu.co y ubíquese en el bloque Entrar, a continuación, ingrese los datos de cuenta: usuario y clave del Portal Virtual.</p>								
+								<p>También puede ingresar digitando la dirección Web http://campus.aunar.edu.co y ubíquese en el bloque Entrar, a continuación, ingrese los datos de cuenta: usuario y clave del Portal Virtual.</p>
 								<br>
 								<strong> 2.	¿Olvidé mi contraseña, qué debo hacer?</strong>
 								
@@ -78,30 +77,26 @@
 								<p>Los cursos del Campus Aunar Virtual hacen parte de programas académicos modalidad virtual (Tecnología y Técnico) que ofrece la Corporación Universitaria Autónoma de Nariño, por lo tanto, no pueden ser matriculados por personal externo a la institución. Es importan mencionar, que las matriculas en línea se pueden realizar a través del link http://inscripciones.aunar.edu.co/ Para Mayor información contáctenos en la siguiente dirección de correo mercadeo@aunar.edu.co o edgar.munoz@aunar.edu.co</p>
 								<br>
 								<strong> 5.	¿En qué momento puedo ingresar al Campus Aunar Virtual?</strong>
-
 								<p>El Campus Aunar Virtual está disponible las 24 horas del día. La hora de ingreso la define el usuario dependiendo de su tiempo y sus ocupaciones. </p>
 								<br>
 								<strong> 6.	¿Cuándo se presenten dificultades para el ingreso a la Plataforma y desarrollo de las actividades que ésta ofrece, con quién puedo comunicarme?</strong>
-
 								<p>Se debe en cuenta el tipo de dificultad que se presenta, es decir, si el inconveniente se debe a olvido de la contraseña deberá remitirse al link: ¿Olvidé mi contraseña, qué debo hacer? Si se trata de una falla técnica deberá contactarse con la persona que administra la Plataforma Virtual a través del siguiente correo edgar.munoz@aunar.edu.co. Por el contrario, si los problemas que usted encuentre están relacionados directamente con los contenidos, actividades y/o evaluaciones deberá comunicarse con el tutor encargado del mismo, por vía celular o correo electrónico; datos que usted conocerá con anterioridad. </p>
 								<br>
 								<strong> 7.	¿Cómo participar en el desarrollo de foros y demás actividades que ofrece el Campus Aunar Virtual?</strong>
-
 								<p>Se recomienda revisar y aplicar las indicaciones del manual de usuario: Manual de usuario </p>
 								<br>
 								<strong> 8.	¿Qué hacer en caso de incumplir los cronogramas de actividades y evaluaciones?</strong>
-
 								<p>En el caso de presentarse dicha situación lo más recomendable es comunicarse con el tutor, manifestando los motivos por los cuales no se cumplió oportunamente con la entrega de las actividades o evaluaciones. Por lo anterior, el estudiante deberá someterse a los criterios que el docente estipule para tal caso, cumpliendo con los nuevos tiempos programados para su presentación. </p>
 								<br>
-							</div>							
+							</div>
 						</div>
 						<div class="tab-pane fade p-3 fondo-gris-os" id="v-pills-messages" role="tabpanel" aria-labelledby="v-pills-messages-tab">
 							<div class="col-12 p-2 px-md-5 text-justify">
-								<strong>Video Introductorio al Campus Aunar Virtual - Modalidad a Distancia</strong>								
+								<strong>Video Introductorio al Campus Aunar Virtual - Modalidad a Distancia</strong>
 							</div>
 							<div class="col-12 p-2 px-md-5 text-center">
 								<br><iframe width="100%" height="450" src="https://www.youtube.com/embed/61j6RbMcbV8" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
-							</div> 
+							</div>
 							
 						</div>
 						<div class="tab-pane fade p-3 fondo-gris-os" id="v-pills-settings" role="tabpanel" aria-labelledby="v-pills-settings-tab">
@@ -125,171 +120,204 @@
 			<div class="row d-flex justify-content-around">
 				<div class="col-12 col-sm-6 col-md-4 col-lg-2">
 					
-						<div class="col-12 p-0 "> <a href="plantas-aromaticas.php" target="plantas-aromaticas"> <img class="mx-auto img-fluid" width="100%" src="img/virtual/1.jpg" ></a></div>
-						<div class="col-12 p-0 ">
-							<div class="p-2 fondo-gris-os fuente-institucional texto-azul-os">
+					<div class="col-12 p-0 content_img_virtual">
+						<a href="plantas-aromaticas.php" target="plantas-aromaticas">
+							<img class="mx-auto img-fluid img_virtual" width="100%" src="img/virtual/1.jpg" >
+							<div class="cont_texto_principal">
+								<div class="texto_principal fuente-institucional f-negrita texto-azul-os">Transformación de Plantas Aromáticas
+								</div>
+							</div>
+							<div class="cont_texto_secundario">
+								<div class="texto_secundario pt-sans texto-azul-os f-negrita">El Técnico Profesional Egresado de la Corporación Universitaria Autónoma de Nariño, esta capacitado para realizar los procesos de limpieza, selección, clasificación, secado, extracción y empaque de plantas ...
+								</div>
+							</div>
+						</a>
+					</div>
+					<div class="col-12 p-0 ">
+						<div class="p-2 fondo-gris-os fuente-institucional texto-azul-os">
 							<br> <strong>Facultad: </strong><br>
 							Agronomía, Veterinaria y Afines
 							<br>
-
 							<br> <strong>Programa: </strong><br>
 							Técnico en la Tranformación de Plantas Aromáticas
 							<br>
-
 							<br> <strong>Código SNIES: </strong><br>
 							101340
 							<br>
-
 							<br> <strong>Título a obtener: </strong><br>
 							Técnico Profesional en la Transformación de Plantas Aromáticas
 							<br>
-
 							<br> <strong>Duración: </strong><br>
 							4 Semestres
 							<br>
-
 							<br> <strong>Modalidad: </strong><br>
 							Virtual
 							<br>
-							</div>
 						</div>
+					</div>
 					
 				</div>
 				<div class="col-12 col-sm-6 col-md-4 col-lg-2">
 					
-						<div class="col-12 p-0 "> <a href="mercadeo-agropecuario.php" target="mercadeo-agropecuario"> <img class="mx-auto img-fluid" width="100%" src="img/virtual/2.jpg" ></a></div>
-						<div class="col-12 p-0 ">
-							<div class="p-2 fondo-gris-os fuente-institucional texto-azul-os">
+					<div class="col-12 p-0 content_img_virtual">
+						<a href="mercadeo-agropecuario.php" target="mercadeo-agropecuario">
+							<img class="mx-auto img-fluid img_virtual" width="100%" src="img/virtual/2.jpg" >
+							<div class="cont_texto_principal">
+								<div class="texto_principal fuente-institucional f-negrita texto-azul-os">Mercadeo Agropecuario
+								</div>
+							</div>
+							<div class="cont_texto_secundario">
+								<div class="texto_secundario pt-sans texto-azul-os f-negrita">Identificar variedades, características, valor nutricional, potencial industrial y de exportación de los productos agropecuarios a comercializar ...
+								</div>
+							</div>
+						</a>
+					</div>
+					<div class="col-12 p-0 ">
+						<div class="p-2 fondo-gris-os fuente-institucional texto-azul-os">
 							<br> <strong> Facultad: </strong> <br>
 							Agronomía, Veterinaria y Afines
 							<br>
-
 							<br> <strong> Programa: </strong> <br>
 							Tecnología en Mercadeo Agropecuario
 							<br>
-
 							<br> <strong> Código SNIES: </strong> <br>
 							101341
 							<br>
-
 							<br> <strong> Título a obtener: </strong> <br>
 							Tecnólogo en Mercadeo Agropecuario
 							<br>
-
 							<br> <strong> Duración: </strong> <br>
 							6 Semestres
 							<br>
-
 							<br> <strong> Modalidad: </strong> <br>
 							Virtual
 							<br>
-							</div>
 						</div>
+					</div>
 					
 				</div>
 				<div class="col-12 col-sm-6 col-md-4 col-lg-2">
 					
-						<div class="col-12 p-0 "> <a href="administracion-empresas-a-distancia.php" target="administracion-empresas"> <img class="mx-auto img-fluid" width="100%" src="img/virtual/3.jpg" ></a></div>
-						<div class="col-12 p-0 ">
-							<div class="p-2 fondo-gris-os fuente-institucional texto-azul-os">
-							<br> <strong>Facultad: </strong><br>
-							Economía, Administración, Contaduría y Afines
-							 <br>
-
-							<br> <strong>Programa: </strong><br>
-							Administración de Empresas
-							<br>
-
-							<br> <strong>Código SNIES: </strong><br>
-							102322
-							<br>
-
-							<br> <strong>Título a obtener: </strong><br>
-							Administrador/a de Empresas
-							<br>
-
-							<br> <strong>Duración: </strong><br>
-							9 Semestres
-							<br>
-
-							<br> <strong>Modalidad: </strong><br>
-							Distancia
-							<br>
+					<div class="col-12 p-0 content_img_virtual"> <a href="administracion-empresas-a-distancia.php" target="administracion-empresas">
+						<img class="mx-auto img-fluid img_virtual" width="100%" src="img/virtual/3.jpg" >
+						<div class="cont_texto_principal">
+							<div class="texto_principal fuente-institucional f-negrita texto-azul-os">Administración de Empresas
 							</div>
 						</div>
-					
+						<div class="cont_texto_secundario">
+							<div class="texto_secundario pt-sans texto-azul-os f-negrita">El profesional egresado del programa de Administración de Empresas de la Corporación Universitaria Autónoma de Nariño, está capacitado para realizar procesos de gestión empresarial ...
+							</div>
+						</div>
+					</a>
 				</div>
-				<div class="col-12 col-sm-6 col-md-4 col-lg-2">
-					
-						<div class="col-12 p-0 "> <a href="contaduria-publica-a-distancia.php" target="contaduria-publica"> <img class="mx-auto img-fluid" width="100%" src="img/virtual/4.jpg" ></a></div>
-						<div class="col-12 p-0 ">
-							<div class="p-2 fondo-gris-os fuente-institucional texto-azul-os">
-							<br> <strong> Facultad: </strong><br>
-							Economía, Administración, Contaduría y Afines
-							<br>
-
-							<br> <strong> Programa: </strong><br>
-							Contaduría Pública
-							<br>
-
-							<br> <strong> Código SNIES: </strong><br>
-							102519
-							<br>
-
-							<br> <strong> Título a obtener: </strong><br>
-							Contador/a Público/a
-							<br>
-
-							<br> <strong> Duración: </strong><br>
-							9 Semestres
-							<br>
-
-							<br> <strong> Modalidad: </strong><br>
-							Distancia
-							<br>
-							</div>
-						</div>
-					
+				<div class="col-12 p-0 ">
+					<div class="p-2 fondo-gris-os fuente-institucional texto-azul-os">
+						<br> <strong>Facultad: </strong><br>
+						Economía, Administración, Contaduría y Afines
+						<br>
+						<br> <strong>Programa: </strong><br>
+						Administración de Empresas
+						<br>
+						<br> <strong>Código SNIES: </strong><br>
+						102322
+						<br>
+						<br> <strong>Título a obtener: </strong><br>
+						Administrador/a de Empresas
+						<br>
+						<br> <strong>Duración: </strong><br>
+						9 Semestres
+						<br>
+						<br> <strong>Modalidad: </strong><br>
+						Distancia
+						<br>
+					</div>
 				</div>
-				<div class="col-12 col-sm-6 col-md-4 col-lg-2">
-					
-						<div class="col-12 p-0 "> <a href="ingenieria-informatica.php" target="ingenieria-informatica"> <img class="mx-auto img-fluid" width="100%" src="img/virtual/5.jpg" ></a></div>
-						<div class="col-12 p-0 ">
-							<div class="p-2 fondo-gris-os fuente-institucional texto-azul-os">
-							<br> <strong>Facultad: </strong>
-							Ingeniería, Arquitectura, Urbanismo y Afines
-							<br>
-
-							<br> <strong>Programa: </strong>
-							Ingeniería Informática
-							<br>
-
-							<br> <strong>Código SNIES: </strong>
-							102883
-							<br>
-
-							<br> <strong>Título a obtener: </strong>
-							Ingeniero/a Informático/a
-							<br>
-
-							<br> <strong>Duración: </strong>
-							9 Semestres
-							<br>
-
-							<br> <strong>Modalidad: </strong>
-							Distancia
-							<br>
+				
+			</div>
+			<div class="col-12 col-sm-6 col-md-4 col-lg-2">
+				
+				<div class="col-12 p-0 content_img_virtual">
+					<a href="contaduria-publica-a-distancia.php" target="contaduria-publica">
+						<img class="mx-auto img-fluid img_virtual" width="100%" src="img/virtual/4.jpg" >
+						<div class="cont_texto_principal">
+							<div class="texto_principal fuente-institucional f-negrita texto-azul-os">Contaduría Pública
 							</div>
 						</div>
-					
-				</div>				
-
+						<div class="cont_texto_secundario">
+							<div class="texto_secundario pt-sans texto-azul-os f-negrita">El Profesional en Contaduría Pública de la Corporación Universitaria Autónoma de Nariño, estará capacitado para el manejo y dirección de la información financiera y contable, con formación humana, ética, mentalidad investigativa, innovadora ...
+							</div>
+						</div>
+					</a>
+				</div>
+				<div class="col-12 p-0 ">
+					<div class="p-2 fondo-gris-os fuente-institucional texto-azul-os">
+						<br> <strong> Facultad: </strong><br>
+						Economía, Administración, Contaduría y Afines
+						<br>
+						<br> <strong> Programa: </strong><br>
+						Contaduría Pública
+						<br>
+						<br> <strong> Código SNIES: </strong><br>
+						102519
+						<br>
+						<br> <strong> Título a obtener: </strong><br>
+						Contador/a Público/a
+						<br>
+						<br> <strong> Duración: </strong><br>
+						9 Semestres
+						<br>
+						<br> <strong> Modalidad: </strong><br>
+						Distancia
+						<br>
+					</div>
+				</div>
+				
+			</div>
+			<div class="col-12 col-sm-6 col-md-4 col-lg-2">
+				
+				<div class="col-12 p-0 content_img_virtual">
+					<a href="ingenieria-informatica.php" target="ingenieria-informatica">
+						<img class="mx-auto img-fluid img_virtual" width="100%" src="img/virtual/5.jpg" >
+						<div class="cont_texto_principal">
+							<div class="texto_principal fuente-institucional f-negrita texto-azul-os">Ingeniería Informática
+							</div>
+						</div>
+						<div class="cont_texto_secundario">
+							<div class="texto_secundario pt-sans texto-azul-os f-negrita">El ingeniero informático egresado de la Corporación Universitaria Autónoma de Nariño es capaz de desarrollar sistemas informáticos innovadores que apliquen conceptos científicos y tecnológicos; formados en principios éticos, sociales ...
+							</div>
+						</div>
+					</a>
+				</div>
+				<div class="col-12 p-0 ">
+					<div class="p-2 fondo-gris-os fuente-institucional texto-azul-os">
+						<br> <strong>Facultad: </strong>
+						Ingeniería, Arquitectura, Urbanismo y Afines
+						<br>
+						<br> <strong>Programa: </strong>
+						Ingeniería Informática
+						<br>
+						<br> <strong>Código SNIES: </strong>
+						102883
+						<br>
+						<br> <strong>Título a obtener: </strong>
+						Ingeniero/a Informático/a
+						<br>
+						<br> <strong>Duración: </strong>
+						9 Semestres
+						<br>
+						<br> <strong>Modalidad: </strong>
+						Distancia
+						<br>
+					</div>
+				</div>
+				
 			</div>
 		</div>
-		
-		
-		<?php include_once('secciones/footer-principal.php');?>
-		
-		
-	</body>
+	</div>
+	
+	
+	<?php include_once('secciones/footer-principal.php');?>
+	
+	
+</body>
 </html>
