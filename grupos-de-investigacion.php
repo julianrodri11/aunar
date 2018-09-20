8ef97d5c1d87d19bd147d84d5a3fa18ed8800cab
Julian Rodriguez <julianrodri11@gmail.com>
2018-09-19 21:28:25 -0500

    Arreglos en grupos de investigación

--- grupos-de-investigacion.php
+++ grupos-de-investigacion.php
@@ -27,15 +27,48 @@
 		
 		
 		<div class="container-fluid texto-azul-os">
-			<div class="row"></div>
+			<div class="row fondo-grupos-investigacion p-5">
+				<div class="col-12 col-md-6 p-2"><img class="img-fluid" width="100%" src="img/investigacion/inmecnar.png" alt="Grupo de Investigacion INMECNAR"></div>
+				<div class="col-12 col-md-6 p-2"><img class="img-fluid" width="100%" src="img/investigacion/irop.png" alt="IROP Mecánica Dental"></div>
+				<div class="col-12 col-md-6 p-2"><img class="img-fluid" width="100%" src="img/investigacion/ciac.png" alt="CIAC Ciencias Administrativas y Contables"></div>
+				<div class="col-12 col-md-6 p-2"><img class="img-fluid" width="100%" src="img/investigacion/zed-matec.png" alt="ZED MATEC Sistemas Expertos, Desarrollo de Software, Matemáticas Aplicadas t TECnología"></div>
+				<div class="col-12 col-md-6 p-2"><img class="img-fluid" width="100%" src="img/investigacion/egresados-aunar.png" alt="Egresados AUNAR"></div>
+				<div class="col-12 col-md-6 p-2"><img class="img-fluid" width="100%" src="img/investigacion/moda-+-cultura.png" alt="MODA-+-CULTURA"></div>
+				<div class="col-12 col-md-6 p-2"><img class="img-fluid" width="100%" src="img/investigacion/metanola.png" alt="METANOLA"></div>
+				<div class="col-12 col-md-6 p-2"><img class="img-fluid" width="100%" src="img/investigacion/gidsma.png" alt="GIDSMA"></div>
+			</div>
+		</div>
+
+		<div class="container-fluid py-5">
+			<div class="row d-flex justify-content-center">
+				<div class="col-8 col-md-4 p-0">
+					<img class="mx-auto img-fluid" width="100%" src="img/investigacion/inmecnar-logo.png" alt="grupos de investigación AUNAR" />
+				</div>
+			</div>
 		</div>
 
 
 
 		<div class="container-fluid">
-			<div class="row">
-				<div class="col-12 pt-lg-5 mt-lg-4"><h3 class="fuente-institucional f-negrita texto-azul-os text-center mx-auto">NIVELES DE FORMACIÓN</h3></div>
-				<!-- <div class="col-12  "><p class="pt-sans f-negrita texto-azul-os text-center mx-auto">NIVELES DE FORMACIÓN</p></div> -->
+			<div class="row fuente-institucional texto-azul-os p-sm-5 d-flex justify-content-center">
+				<div class="col-9 pt-lg-5 mt-lg-4 f-myriad text-justify">					
+				<p>
+					<br> <strong>Descripción:</strong><br>
+					Un Grupo de Investigación es la unidad docente especializada que se dedica a la investigación científica, al desarrollo tecnológico o a la innovación, integrada por profesores, jóvenes investigadores y otro tipo de personal interno y externo de la institución, provenientes de una o de diferentes áreas del conocimiento. A nivel institucional, se requiere que estos procesos tengan en cuenta, para su formulación y desarrollo, los lineamientos de Colciencias. 
+				</p>
+				<p>
+					<br> <strong>Misión</strong><br>
+					Adelantar procesos de investigación científica, de innovación o desarrollo tecnológico y de desarrollo sostenible, como respuesta a las necesidades institucionales o del entorno; en articulación con los procesos institucionales de formación académica, de proyección social y de producción de conocimiento.
+				</p>
+				<p>
+					<br> <strong>Visión</strong><br>
+					Ser líderes a nivel regional en el desarrollo de proyectos de innovación, desarrollo tecnológico y desarrollo sostenible, en el marco de la alianza universidad - empresa - estado, para 2020.
+				</p>
+				<p>
+					<br> <strong>Historia </strong><br>
+					El grupo de investigación del programa de Ingeniería Mecánica de la Universidad Autónoma de Nariño AUNAR, se formalizó a finales del año 2010, con el objeto de dar solución a problemas de algunas empresas de nuestro departamento, tal es el caso de la industria panadera, la cual a través de ACOPI, presentaron como problema la ineficiencia en la utilización de los huevos, ya que estos son unos de los elementos de más consumo en esta industria. Por tal razón, se analizó dicho problema y se empezó a plantear todos los respectivos pasos secuenciales para dar la solución. En este proceso, también se vinculó a un grupo de estudiantes de semestres superiores, pensando en la idea de que vayan desarrollando en sentido práctico su carrera y a la vez empiecen a aportar con soluciones a la problemática de la región; en este sentido se está no solo cumpliendo con uno de los pilares de la educación como lo es LA INVESTIGACIÓN sino también se propende por direccionar el camino del futuro profesional.
+				</p>
+				</div>
 			</div>			
 		</div>
 
