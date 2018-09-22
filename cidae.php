6639ae3a5a0f2af45a08f4f881bee0a582423e05
Julian Rodriguez <julianrodri11@gmail.com>
2018-09-22 00:00:02 -0500

    CIDAE

--- cidae.php
+++ cidae.php
@@ -33,7 +33,7 @@
 					<div class="nav flex-column nav-pills" id="v-pills-tab" role="tablist" aria-orientation="vertical">
 						<!-- pestaña 1 -->
 						<a class="nav-link fuente-institucional texto-azul-os sin-texto-subrayado hvr-underline-from-center f-negrita fondo-gris-os active" id="v-pills-home-tab" data-toggle="pill" href="#v-pills-home" role="tab" aria-controls="v-pills-home" aria-selected="true">
-							Quiénes Somos
+							¿Quiénes Somos?
 						</a>
 						
 						<!-- pestaña 2 -->
@@ -78,81 +78,139 @@
 						
 						<!-- pestaña 1 -->
 						<div class="tab-pane fade p-3 fondo-gris-os show active" id="v-pills-home" role="tabpanel" aria-labelledby="v-pills-home-tab">
-							<div class="col-12 p-2 px-md-5 text-justify">
-								<p>Se entiende por primeros auxilios, la atención inmediata y temporal que se brinda a víctimas de accidentes o de afecciones de salud que se presentan repentinamente y que ponen en riesgo el bienestar de la persona. Este servicio se presta sin costo, a cualquier persona que se encuentre en el campus universitario y que requiera de dicha atención.</p>
-								<p>Cuando se requiere este servicio, por favor acérquese a la Oficina de Primeros Auxilios ubicada en el primer piso de Institución.</p>
+							<div class="col-12 p-2 px-md-5 text-center">
+								<span class="text-center pt-sans"> <strong>CENTRO DE INVESTIGACIONES DESARROLLO Y ASESORÍA EMPRESARIAL <br>"CIDAE"</strong></span>
 							</div>
-							<div class="col-12 p-2 px-md-5"><img class="img-fluid" width="100%" src="img/bienestar/aunar_primeros_auxilios.jpg"></div>
+							<div class="col-12 p-2 px-md-5 text-justify">								
+								<p>
+									La Corporación Autónoma de Nariño, AUNAR, como comunidad productora de conocimiento crítico y aplicable a las necesidades regionales y nacionales, centra su misión en la docencia, la investigación y la extensión, asumiendo un claro compromiso con el desarrollo de Nariño.
+									<br><br>
+									A nivel institucional se define al centro de Investigación como: "La unidad de servicio en donde la actividad investigativa procura la aproximación de las disciplinas científicas y la unión de la investigación y la docencia". Orgánicamente estará orientado y constituido por unidades especializadas en potenciar el emprendimiento, la difusión académica y la conformación de semilleros y grupos de investigación.
+									<br><br>
+									Por esta razón y de acuerdo a la visión y misión, la investigación en la AUNAR, se concibe como un proceso permanente de producción de conocimiento, generador de competencias que permiten al estudiante y al egresado desenvolverse en distintos campos con pertinencia y capacidad creadora, con la habilidad de hacer, de ser y retroalimentar su conocimiento a través de la práctica profesional.
+									<br><br>
+									Como principios de la investigación en la institución se identifican:
+									<br><br>
+									
+									- La articulación entre docente - estudiante orientada a la investigación.
+									<br>
+									- Excelencia en la búsqueda del conocimiento y desarrollo integral de la región.
+									<br>
+									- Trascendencia de lo local a lo internacional.
+									<br>
+									- Actualización de enfoques, tendencias, teorías y métodos
+									<br>
+									- Ética respecto a lo técnico y a lo científico.
+									<br><br>
+									Es así como se sustenta la existencia del CENTRO DE INVESTIGACIONES, DESARROLLO Y ASESORÍA EMPRESARIAL. CIDAE, estamento, comprometido con el desarrollo teórico y práctico de la actividad investigativa, así como con el desarrollo de acciones de proyección social, que se encaminan a la articulación del quehacer universitario con el contexto y que se concretiza con la asesoría y la planeación de actividades que convocan al sector empresarial, permiten su desarrollo y plantea alternativas que redundan en su desarrollo, en el aprendizaje, en la imagen corporativa de la institución universitaria y en el desarrollo regional.
+									<br><br>
+									La naturaleza y la esencia misma del CIDAE, obliga a establecer dos pilares fundamentales que retroalimentan permanentemente su dinámica y su quehacer en la vida universitaria: El campo de la investigación y el campo de lo empresarial, los cuales permiten la articulación de la docencia, la investigación misma y la proyección social.
+									<br><br>
+								</p>
+							</div>							
 						</div>
 						
 						<!-- pestaña 2-->
 						<div class="tab-pane fade p-3 fondo-gris-os" id="v-pills-profile" role="tabpanel" aria-labelledby="v-pills-profile-tab">
 							<div class="col-12 p-2 px-md-5 text-justify">
-								<p>La hipertensión arterial es una patología crónica que consiste en el aumento de la presión arterial. Una de las características de esta enfermedad es que no presenta síntomas claros y no se manifiestan durante mucho tiempo.</p>
-								<p>Para una persona hipertensa, es indispensable que controle sus valores de presión sanguínea varias veces por semana. Caso contrario, podría presentar complicaciones silenciosas debido al efecto que produce la hipertensión arterial sobre muchos órganos.</p>
-								<p>¿Cómo controlar la presión arterial?</p>
-								<p>Las siguientes medidas pueden contribuir al control de la presión arterial:</p>
-								<p> - Perder peso
-									- Aumentar la actividad física <br>
-									- Limitar la ingesta de sal <br>
-									- Limitar la ingesta de alcohol <br>
-									- Dejar de fumar <br>
-								- Tomar la medicación aconsejada por tu médico</p>
-								<p>Cuando se requiere este servicio, por favor acérquese a la Oficina de Primeros Auxilios ubicada en el primer piso de Institución.</p>
+								<p class="fuente-institucional">
+									<strong>EL CENTRO DE INVESTIGACIONES, DESARROLLO Y ASESORÍA EMPRESARIAL CIDAE</strong>, es un estamento comprometido con el desarrollo teórico práctico de la actividad investigativa de tal manera que sea factible generar, desarrollar y fomentar en el estudiante y en el docente una actitud crítica, reflexiva y creativa frente al conocimiento y al papel desempeñado dentro del contexto y de su propio criterio profesional.
+
+								</p>
 							</div>
-							<div class="col-12 p-2 px-md-5"><img class="img-fluid" width="100%" src="img/bienestar/tension_arterial.jpg"></div>
 						</div>
 						
 						<!-- pestaña 3-->
 						<div class="tab-pane fade p-3 fondo-gris-os" id="v-pills-messages" role="tabpanel" aria-labelledby="v-pills-messages-tab">
 							<div class="col-12 p-2 px-md-5 text-justify">
-								<p>Auto cuidado y manejo responsable de la sexualidad, derechos sexuales y reproductivos, conocimiento de Infecciones de Transmisión Sexual, su prevención y el manejo de ellas con asesoría en enfermería. <br>
-									- Higiene íntima. <br>
-									- Asesoría en planificación familiar. <br>
-									- Autoexamen de mamas
+								<p class="fuente-institucional">
+									Institucionalmente el CIDAE, promoverá la investigación en todos los programas de las distintas facultades, para que desde un lenguaje investigativo unificado, fortalezca el desarrollo y ejecución de trabajos de grado y de las distintas formas de investigación tanto para el personal docente como estudiantes con visión científica que satisfaga el interés personal y el servicio a la comunidad; promoverá a partir de la investigación el desarrollo de tecnologías, la creación y consolidación de una comunidad investigativa y se encaminará al reconocimiento de la AUNAR como un ente investigador por excelencia que presenta resultados acordes con las necesidades de la región.
 								</p>
-							</div>
-							<div class="col-12 p-2 px-md-5"><img class="img-fluid" width="100%" src="img/bienestar/asesorias_educacion_sexual.jpg"></div>
+							</div>							
 						</div>
 						
 						<!-- pestaña 4-->
 						<div class="tab-pane fade p-3 fondo-gris-os" id="v-pills-settings" role="tabpanel" aria-labelledby="v-pills-settings-tab">
 							<div class="col-12 p-2 px-md-5 text-justify">
-								Es un programa que informa y educa a la comunidad universitaria sobre la importancia que tiene el respeto consigo mismo, con el auto cuidado y el aprendizaje significativo de estilos de vida que favorezcan la salud física y mental.
-							</div>
-							<div class="col-12 p-2 px-md-5"><img class="img-fluid" width="100%" src="img/bienestar/promocion_y_prevencion.jpg"></div>
+								<strong>Objetivo general</strong>
+								
+								<p>
+								Promover y fortalecer el desarrollo de la actividad investigativa al interior de la AUNAR, con el fin de lograr la formación de estudiantes con visión crítica y emprendedora y la construcción de una comunidad investigadora, reconocida en el medio, que genere resultados necesarios en la región.
+								<br><br>
+								</p>
+								<strong>Objetivos específicos</strong>
+								<br>
+								<p>
+								Contribuir en la planeación y ejecución de proyectos de investigación e ideas de negocios que generen desarrollo para la institución y la comunidad general.
+								<br><br>
+								Brindar espacios para la formación investigativa de los estudiantes de la AUNAR a través de la firma de convenios de cooperación institucional para la práctica empresarial.
+								<br><br>
+								Propiciar espacios de capacitación y apoyo al desarrollo de grupos de investigación disciplinar y formal en la AUNAR de Nariño y su gestión con el entorno socioeconómico, cultural e investigativo de las regiones. Orientar el proceso investigativo formativo de los estudiantes en todas las facultades.
+								<br><br>
+								Promover y orientar el ejercicio docente de investigación en y desde el aula como elemento básico para la investigación en sentido estricto.
+								<br><br>
+								Crear instrumentos que permitan difundir los aportes de carácter investigativo que los docentes y estudiantes realicen.
+								<br><br>
+								Fomentar la formación de colectivos que permitan el abordaje del conocimiento desde una visión interdisciplinaria.
+								<br><br>
+								Diseñar, organizar y supervisar los programas y líneas de investigación en la AUNAR.
+
+								</p>
+							</div>							
 						</div>
 						
 						<!-- pestaña 5-->
 						<div class="tab-pane fade p-3 fondo-gris-os" id="v-pills-act-deportivas" role="tabpanel" aria-labelledby="v-pills-act-deportivas-tab">
 							<div class="col-12 p-2 px-md-5 text-justify">
-								<p>Durante la realización de cualquier actividad deportiva y cultural AUNAR, se tiene acompañamiento de personal experto en cada área y que garantice las condiciones óptimas en cada evento.</p>
+								<p>
+									Las líneas de Investigación son los enfoques disciplinarios establecidos por la institución, que permiten enmarcar los procesos, prácticas y perspectivas de análisis de los proyectos de investigación.
+									<br><br>
+									Las líneas de investigación establecen la relación entre la teoría y la práctica; se convierte en el orientador que articulada la formación, la investigación, la proyección, el desarrollo de competencias profesionales y la posibilidad de generar aportes a los diversos contextos; permite el diseño de estrategias conducentes a generar procesos de desarrollo empresarial y a la creación de nuevas empresas, alternativas necesarias para contribuir desde la academia al desarrollo local, nacional e internacional
+
+									<br><br>									
+									
+									<div class="col-12 text-center"><h3 class=" f-negrita fuente-institucional texto-azul">Líneas de Investigación Institucionales</h3> </div> 			
+									<div class="col-12 texto-azul">Para consultar líneas de investigación</div>
+									<div class="col-12"><a class="btn text-light fondo-azul-oscuro pt-sans hvr-forward" href="http://cidae.aunar.edu.co/images/Documentos/LineasInvestigacion.pdf" target="_lineas_i">Click aquí</a> </div>
+
+								</p>
 							</div>
-							<div class="col-12 p-2 px-md-5"><img class="img-fluid" width="100%" src="img/bienestar/acompanamiento_actividades_deportivas.jpg"></div>
+							
 						</div>
 
 						<!-- pestaña 6-->
 						<div class="tab-pane fade p-3 fondo-gris-os" id="v-pills-act-6" role="tabpanel" aria-labelledby="v-pills-act-6-tab">
 							<div class="col-12 p-2 px-md-5 text-justify">
-								<p>6666 Durante la realización de cualquier actividad deportiva y cultural AUNAR, se tiene acompañamiento de personal experto en cada área y que garantice las condiciones óptimas en cada evento.</p>
-							</div>
-							<div class="col-12 p-2 px-md-5"><img class="img-fluid" width="100%" src="img/bienestar/acompanamiento_actividades_deportivas.jpg"></div>
+							<p>Como principios de la investigación en la institución se identifican:
+							<br><br>
+							- La articulación entre docente - discente e investigación.
+							<br>
+							- Excelencia en la búsqueda del conocimiento y desarrollo integral de la región.
+							<br>
+							- Trascendencia de lo local a lo internacional. 
+							<br>
+							- Actualización de enfoques, tendencias, teorías y métodos.
+							<br>
+							- Ética respecto a lo técnico y a lo científico.
+							<br><br>
+							</p>
+							</div>							
 						</div>
 
 						<!-- pestaña 7-->
 						<div class="tab-pane fade p-3 fondo-gris-os" id="v-pills-act-7" role="tabpanel" aria-labelledby="v-pills-act-7-tab">
-							<div class="col-12 p-2 px-md-5 text-justify">
-								<p>7777 Durante la realización de cualquier actividad deportiva y cultural AUNAR, se tiene acompañamiento de personal experto en cada área y que garantice las condiciones óptimas en cada evento.</p>
+							<div class="col-12 p-2 px-md-5 text-center">
+								<h3 class=" f-negrita fuente-institucional texto-azul">Organigrama CIDAE AUNAR</h3>  
 							</div>
-							<div class="col-12 p-2 px-md-5"><img class="img-fluid" width="100%" src="img/bienestar/acompanamiento_actividades_deportivas.jpg"></div>
+							<div class="col-12 p-2 px-md-5"><img class="img-fluid" width="100%" src="img/investigacion/organigrama-cidae-aunar.jpg" alt="organigrama cida aunar"></div>
 						</div>
 
 						<!-- pestaña 8-->
 						<div class="tab-pane fade p-3 fondo-gris-os" id="v-pills-act-8" role="tabpanel" aria-labelledby="v-pills-act-8-tab">
-							<div class="col-12 p-2 px-md-5 text-justify">
-								<p>888 Durante la realización de cualquier actividad deportiva y cultural AUNAR, se tiene acompañamiento de personal experto en cada área y que garantice las condiciones óptimas en cada evento.</p>
+							<div class="col-12 p-2 px-md-5 text-center">
+								<h3 class=" f-negrita fuente-institucional texto-azul">Formatos</h3>  
 							</div>
-							<div class="col-12 p-2 px-md-5"><img class="img-fluid" width="100%" src="img/bienestar/acompanamiento_actividades_deportivas.jpg"></div>
+							<div class="col-12 p-2 px-md-5"><img class="img-fluid" width="100%" src="img/investigacion/organigrama-cidae-aunar.jpg" alt="organigrama cida aunar"></div>
 						</div>
 
 					</div>
