436fc01d74201c5aa0d00eb792b537eaf8e67f8d
Julian Rodriguez <julianrodri11@gmail.com>
2018-09-24 22:41:00 -0500

    Publicaciones completa

--- otri.php
+++ otri.php
@@ -130,6 +130,7 @@
 			</div>
 			
 		</div>
+		
 		<?php include_once('secciones/footer-principal.php');?>
 		
 		
--- publicaciones.php
+++ publicaciones.php
@@ -26,11 +26,135 @@
 		<?php include_once('secciones/menu2.php');?>
 		<?php include_once('secciones/menu_investigacion.php');?>
 		
+		<!-- <div class="container-fluid">
+				<div class="row">
+					<div class="col-12 col-md-6 p-0 fondo-gris-os">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad dolor nemo rerum illum repellendus beatae expedita numquam? Facilis eius placeat eos commodi nemo, illo nulla cupiditate iure nesciunt veritatis aperiam id debitis sit iste, sint cumque cum nam laboriosam. Aut sunt inventore asperiores! Perferendis voluptatem accusamus ratione, culpa fugit eveniet.</div>
+					<div class="col-12 col-md-6 p-0">Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis delectus, sed quae saepe, nam doloremque similique in at ducimus ratione unde obcaecati repudiandae molestiae dolore voluptate sunt repellendus optio quam, recusandae neque perferendis fugiat facilis assumenda quod! Deserunt, nesciunt officia asperiores, aut, est voluptates sapiente illo culpa nam vitae eligendi!</div>
+				</div>
+		</div> -->
+		<!-- personal encargado -->
+		
 		<div class="container-fluid">
-			<div class="row">
-			<div class="col-12 col-md-6 p-0 fondo-gris-os">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad dolor nemo rerum illum repellendus beatae expedita numquam? Facilis eius placeat eos commodi nemo, illo nulla cupiditate iure nesciunt veritatis aperiam id debitis sit iste, sint cumque cum nam laboriosam. Aut sunt inventore asperiores! Perferendis voluptatem accusamus ratione, culpa fugit eveniet.</div>
-			<div class="col-12 col-md-6 p-0">Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis delectus, sed quae saepe, nam doloremque similique in at ducimus ratione unde obcaecati repudiandae molestiae dolore voluptate sunt repellendus optio quam, recusandae neque perferendis fugiat facilis assumenda quod! Deserunt, nesciunt officia asperiores, aut, est voluptates sapiente illo culpa nam vitae eligendi!</div>
+			
+			<!-- publicación 1 -->
+
+			<div class="row py-5 py-md-0">
+				
+				<!-- imagen de publicación -->
+				<div class="col-12 col-lg-6 d-flex justify-content-center align-items-center fondo-gris-os">
+					<div class="row col-12 ">
+						<div class="col-12 py-3 text-center mx-auto ">
+							<!-- aqui va todo lo que se quiera centrar -->
+							<img src="img/investigacion/publicacion-1.jpg" alt="publicaciones AUNAR" width="80%">
+						</div>
+					</div>
+				</div>
+				<!-- fin imagen de  publicación -->
+
+				<!-- información publicación -->
+				<div class="col-12 col-lg-6 d-flex justify-content-center align-items-center">
+					<div class="row col-12 ">
+						<div class="col-12 py-3 mx-auto texto-azul-os ">
+							<!-- aqui va todo lo que se quiera centrar -->
+							<div class="text-center pr-lg-5">
+								<h1 class="texto-amarillo fuente-institucional f-negrita fuente-big p-0"><strong>AUNAR TECH</strong></h1>
+							</div>							
+							<div class="pt-sans text-justify pr-lg-5">
+								La “Revista Virtual de la Facultad de Ingeniería AUNAR es una
+								publicación académica e investigativa de la Corporación Universitaria
+								Autónoma de Nariño, comprometida con la cultura, la ciencia, la
+								investigación y el desarrollo regional...
+							</div>
+							<div class="pt-sans text-justify pr-lg-5 py-lg-5">
+								<a href="#" class="p-2 border borde-azul badge badge-pill hover-amarillo hover-texto-blanco float-right texto-azul sin-texto-subrayado fuente-institucional">Leer Más</a>
+							</div>
+						</div>
+					</div>
+				</div>
+				<!-- fin información publicación -->
 			</div>
+			<!-- fin publicación 1 -->
+
+
+			<!-- publicación 2 -->
+			<div class="row py-5 py-md-0">
+
+				<!-- información personal -->
+				<div class="col-12 col-lg-6 d-flex justify-content-center align-items-center p-0">
+					<div class="row col-12 ">
+						<div class="col-12 p-0 py-md-3 mx-auto texto-azul-os ">
+							<!-- aqui va todo lo que se quiera centrar -->
+							<div class="text-center pr-lg-5">
+								<h1 class="texto-amarillo fuente-institucional fuente-18 p-0">Boletín de Investigación</h1>
+								<h1 class="texto-amarillo fuente-institucional fuente-big p-0"><strong>CREAN</strong></h1>
+							</div>							
+							<div class="pt-sans text-justify pr-lg-5">
+								Al revisar el concepto de la ciencia se puede comprender que la misión
+								de la misma, es hacer un aporte de conocimientos para que los seres
+								humanos puedan comprender de mejor ...
+							</div>
+							<div class="pt-sans text-justify pr-lg-5 py-lg-5">
+								<a href="#" class="p-2 border borde-azul badge badge-pill hover-amarillo hover-texto-blanco float-right texto-azul sin-texto-subrayado fuente-institucional">Leer Más</a>
+							</div>
+						</div>
+					</div>
+				</div>
+				<!-- imagen de publicación -->
+				<div class="col-12 col-lg-6 d-flex justify-content-center align-items-center fondo-gris-os p-0">
+					<div class="row col-12 p-0">
+						<div class="col-12 p-0 text-center mx-auto ">
+							<!-- aqui va todo lo que se quiera centrar -->
+							<img src="img/investigacion/publicacion-2.jpg" alt="publicaciones AUNAR" width="100%">
+						</div>
+					</div>
+				</div>
+				<!-- fin imagen de publicación -->
+
+				
+			</div>
+			<!-- fin publicación 2 -->
+
+			<!-- publicación 3 -->
+
+			<div class="row py-5 py-md-0">
+				
+				<!-- imagen de publicación -->
+				<div class="col-12 col-lg-6 d-flex justify-content-center align-items-center fondo-gris-os">
+					<div class="row col-12 ">
+						<div class="col-12 py-3 text-center mx-auto ">
+							<!-- aqui va todo lo que se quiera centrar -->
+							<img src="img/investigacion/publicacion-3.jpg" alt="publicaciones AUNAR" width="100%">
+						</div>
+					</div>
+				</div>
+				<!-- fin imagen de  publicación -->
+
+				<!-- información publicación -->
+				<div class="col-12 col-lg-6 d-flex justify-content-center align-items-center">
+					<div class="row col-12 ">
+						<div class="col-12 py-3 mx-auto texto-azul-os ">
+							<!-- aqui va todo lo que se quiera centrar -->
+							<div class="text-center pr-lg-5">
+								<h1 class="texto-amarillo fuente-institucional fuente-18 p-0">Revista Facultad de</h1>
+								<h1 class="texto-amarillo fuente-institucional fuente-big p-0"><strong>Ciencias Administrativas y Contables</strong></h1>
+							</div>								
+							<div class="pt-sans text-justify pr-lg-5">
+								Es un documento que presenta la competencia comunicativa de
+								estudiantes y docentes de la facultad a través de una revista digital la cual
+								permite exponer apreciaciones...
+							</div>
+							<div class="pt-sans text-justify pr-lg-5 py-lg-5">
+								<a href="#" class="p-2 border borde-azul badge badge-pill hover-amarillo hover-texto-blanco float-right texto-azul sin-texto-subrayado fuente-institucional">Leer Más</a>
+							</div>
+						</div>
+					</div>
+				</div>
+				<!-- fin información publicación -->
+			</div>
+			<!-- fin publicación 3 -->
+
+
+			
 		</div>
 		
 		
