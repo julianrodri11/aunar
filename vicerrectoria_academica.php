211d8803d7ae231af6b84a1335a968379728635f
Julian Rodriguez <julianrodri11@gmail.com>
2018-09-25 18:05:07 -0500

    Se agrega información a vicerrectoria académica

--- vicerrectoria_academica.php
+++ vicerrectoria_academica.php
@@ -73,9 +73,151 @@
 		<!-- Fin slider-->
 		
 
+		<div class="container-fluid">
+			<div class="row d-flex justify-content-center fondo-gris-os">
+				<div class="col-10 col-lg-8 p-3 texto-azul-os">
+					<div class="col-12 text-center">
+						<h3 class="fuente-institucional"><strong>Descripción de la Vicerrectoría Administrativa y Financiera</strong></h3>	
+					</div>
+					<div class="col-12 text-justify border-bottom borde-amarillo">
+						<p class="pt-sans">
+							Gerencia los programas de docencia, investigación y extensión, para hacer cumplir los propósitos de la misión, visión y filosofía institucional
+							<br>
+							Dirige y evalúa la planeación, organización y desarrollo de los procesos de ingreso, control y egreso de los estudiantes de la corporación, así como dirigir y evaluar las actividades de control y seguimiento de las diferentes facultades
+						</p>						
+					</div>
+
+					<div class="col-12">
+						<h3 class="fuente-institucional"><strong>Objetivos</strong></h3>	
+					</div>
+					<div class="col-12 text-justify ">
+						<p class="pt-sans">
+							<br><br>-Planear, dirigir y ejecutar de común acuerdo con el rector, las políticas sobre asuntos académicos de la Corporación Universitaria
+							<br><br>-Coordinar las actividades académicas de las unidades, así como la evaluación periódica de los diferentes programas de estudio y proponer las
+							modificaciones que estime conveniente
+							<br><br>-Planear los objetivos del área, liderar el trabajo de equipo y evaluar los
+							resultados alcanzados
+							<br><br>-Organizar y dirigir las dependencias a su cargo.
+							<br><br>-Velar por el cumplimiento de los reglamentos o estatutos académicos,
+							estudiantiles y profesorales vigentes.
+							<br><br>-Vigilar el programa de investigación de la institución.
+							<br><br>-Elaborar en coordinación de la vicerrectoría administrativa el calendario para cada periodo académico, así como los instructivos y formatos
+							necesarios para orientar a los estudiantes en sus trámites administrativos y académicos
+							<br><br>-Vigilar la ejecución de actividades de orden académico, elaboración de reglamentos y supervisar la aplicación de programas académicos con
+							los directores de programa
+							<br><br>-Organizar y supervisar los asuntos relacionados con admisiones y registro académico, biblioteca, publicaciones, material didáctico e
+							investigaciones.
+							<br><br>-Coadyuvar con el coordinador de personal de selección en la selección, reclutamiento e inducción del personal docente de nuevo ingreso
+							<br><br>-En coordinación con las decanaturas, dar seguimiento y retroalimentar a los profesores en su desempeño docente con base en las
+							evaluaciones y procedimientos respectivos
+							<br><br>-Asesorar a la Vicerrectoría Administrativa en la revisión, desarrollo y selección de material bibliográfico para el desempeño exitoso de los
+							programas académicos
+							<br><br>-Supervisar la consistencia de la información relacionada con el pensum académico desarrollado en la diferentes en las diferentes facultades
+							<br><br>-Supervisar la impresión y entrega oportuna de listas de los estudiantes a las decanaturas correspondiente, al inicio del periodo académico
+							<br><br>-Elaborar de manera coordinada con las decanaturas, los horarios de clase y atender cualquier asunto académico que se presente durante el
+							periodo académico
+							<br><br>-Coordinar conjuntamente con las decanaturas, las actividades de asesoría académica para los estudiantes
+							<br><br>-Dar el seguimiento necesario a los reportes de incidentes críticos de conducta y académicos que reciba de las decanaturas
+							<br><br>-Supervisar a las decanaturas en el siguiente a las actividades de los egresados a fin de enriquecer los procesos internos de evaluación
+							institucional
+							<br><br>-Asistir a la coordinación de personal en la detección de los elementos mínimos de didáctica que debe tener el profesor en la detección de los
+							elementos mínimos de didáctica que debe tener el profesor candidato y de las áreas susceptibles de mejora por medio de la capacitación
+							<br><br>-Supervisar los procesos de admisión, inscripción y reinscripción, exámenes ordinarios, extraordinarios y cursos de verano, con base en los
+							calendarios académicos
+							<br><br>-Supervisar que el sistema de registro académico BISEL permanezca actualizado en la captura de planes de estudio, planta docente y
+							alumnado de la Corporación Autónoma de Nariño
+							<br><br>-Supervisar la captura, elaboración, registro y cotejo de las listas de notas de exámenes ordinarios, supletorios, así como las rectificaciones en
+							el sistema de registro académico
+							<br><br>-Firmar, supervisar y controlar la expedición de documentos como: constancias de estudio, certificados de notas, resoluciones de homologación,
+							traslados y transferencias, títulos, diplomas y menciones honoríficas
+							<br><br>-Supervisar y controlar el proceso de solicitud y autorización de exámenes supletorios, habilitaciones y validaciones, así como el registro de
+							los libros de calificaciones
+							<br><br>-Supervisar que los boletines de calificación sean impresos inmediatamente después de cada periodo académico
+							<br><br>-Supervisar la atención e información dada por el personal de registro y control académico a los estudiantes e intervenir en aquellos asuntos
+							que no pueden ser resueltos en esta primera instancia
+							<br><br>-Supervisar, controlar y custodiar los expedientes de los estudiantes a fin de que su documentación esté debidamente integrada y cumpla con
+							los requisitos legales
+							<br><br>-Estudiar, analizar y proponer alternativas de solución a problemas relacionados con el desarrollo docente y estudiantil
+							<br><br>-Colaborar en el análisis y elaboración del reglamento general y los reglamentos internos, a fin de que esté en concordancia con los
+							lineamientos generales de la institución
+						</p>						
+					</div>
+					
+				</div>
+				
+			</div>
+		</div>
 		
+		<!-- personal encargado -->
+	<div class="container-fluid py-5">
 		
-		<!-- personal encargado -->		
+		<div class="row ">
+			
+			<!-- imagen de personal -->
+			<div class="col-12 col-lg-6 d-flex justify-content-center align-items-center">
+				<div class="row col-12 ">
+					<div class="col-12 py-3 text-center text-lg-right mx-auto ">
+						<!-- aqui va todo lo que se quiera centrar -->
+						<img src="img/institucional/constanza_colunge_ordonez.png" alt="CONSTANZA COLUNGE ORDÓÑEZ" width="35%">
+					</div>
+				</div>
+			</div>
+			<!-- fin imagen de personal -->
+			<!-- información personal -->
+			<div class="col-12 col-lg-6 d-flex justify-content-center align-items-center">
+				<div class="row col-12 ">
+					<div class="col-12 py-3 mx-auto text-left texto-azul-os ">
+						<!-- aqui va todo lo que se quiera centrar -->
+						<h4 class="texto-amarillo fuente-institucional f-negrita p-0">PROFESIONAL ENCARGADO</h4>
+						<h4 class="texto-amarillo fuente-institucional f-negrita p-0">RECTOR (A)</h4>
+						<div class="pt-sans">
+							<label class="f-negrita">CONSTANZA COLUNGE ORDÓÑEZ</label>
+							<br>MAESTRÍA EN E-LEARNING
+							<br>-12 AÑOS EN Experiencia Académica
+							<br>-722 6020 EXTENSION 414 - 
+							<br><br>academico@aunar.edu.co
+							
+						</div>
+					</div>
+				</div>
+			</div>
+			<!-- fin información personal -->
+		</div>
+		<!-- linea amarilla -->
+		<div class="row">
+			<div class="col-8 borde-amarillo border-bottom p-0 mx-auto py-3"></div>
+		</div>
+		<div class="row py-5">
+			
+			<!-- imagen de personal -->
+			<div class="col-12 col-lg-6 d-flex justify-content-center align-items-center">
+				<div class="row col-12 ">
+					<div class="col-12 py-3 text-center text-lg-right mx-auto">
+						<!-- aqui va todo lo que se quiera centrar -->
+						<img src="img/institucional/secretaria.png" alt="Secretaria vicerrectoria académica" width="35%">
+					</div>
+				</div>
+			</div>
+			<!-- fin imagen de personal -->
+			
+			<!-- información personal -->
+			<div class="col-12 col-lg-6 d-flex justify-content-center align-items-center">
+				<div class="row col-12 ">
+					<div class="col-12 py-3 mx-auto text-center text-lg-left texto-azul-os">
+						<!-- aqui va todo lo que se quiera centrar -->
+						<h4 class="texto-amarillo fuente-institucional f-negrita p-0">SECRETARIA</h4>
+						<div class="pt-sans">
+							<label class="f-negrita"></label>
+							<br>auxiliar.vacademica@aunar.edu.co
+						</div>
+					</div>
+				</div>
+			</div>
+			<!-- fin información personal -->
+		</div>
+		
+	</div>
+	<!-- fin personal encargado -->
 
 		
 
