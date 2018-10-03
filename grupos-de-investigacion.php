fc71abebc9c999ca80ffa87eb5003dc330ed56d0
Julian Rodriguez <julianrodri11@gmail.com>
2018-10-02 19:55:44 -0500

    Adiciones a grupos de investigación

--- grupos-de-investigacion.php
+++ grupos-de-investigacion.php
@@ -55,7 +55,7 @@
 			</div>
 		</div>
 		
-		<!-- imágen de inmecnar -->
+		<!-- imágen de grupo de investigación  -->
 		<div class="container-fluid py-5">
 			<div class="row d-flex justify-content-center">
 				<div class="col-8 col-md-4 p-0">
@@ -82,13 +82,13 @@
 				<div class="col-8 col-md-7 border-bottom borde-azul fondo-gris-os p-2">
 					<div class="row">
 						<div class="col-12 col-sm-6 text-left text-sm-right">Site:</div>
-						<div class="col-12 col-sm-6">Pendiente</div>
+						<div class="col-12 col-sm-6 bg-danger">Pendiente</div>
 					</div>
 				</div>
 				<div class="col-8 col-md-7 border-bottom borde-azul fondo-gris-c  p-2">
 					<div class="row">
 						<div class="col-12 col-sm-6 text-left text-sm-right">Código en Colciencias:</div>
-						<div class="col-12 col-sm-6">Pendiente</div>
+						<div class="col-12 col-sm-6 bg-danger">Pendiente</div>
 					</div>
 				</div>
 				<div class="col-8 col-md-7 border-bottom borde-azul fondo-gris-os p-2">
@@ -106,6 +106,7 @@
 			</div>
 		</div>
 		
+		<!-- Datos detallados de grupo de investigación -->
 		<div class="container-fluid">
 			<div class="row fuente-institucional texto-azul-os p-sm-5 d-flex justify-content-center">
 				<div class="col-9 pt-lg-3 mt-lg-4 f-myriad text-justify">
@@ -128,6 +129,163 @@
 				</div>
 			</div>
 		</div>
+
+		<!-- imágen de inmecnar -->
+		<div class="container-fluid py-5">
+			<div class="row d-flex justify-content-center">
+				<div class="col-8 col-md-7 p-0">
+					<img class="mx-auto img-fluid" width="100%" src="img/investigacion/metanola.png" alt="grupos de investigación metanola AUNAR" />
+				</div>
+			</div>
+		</div>
+		
+		<!-- Datos de grupo de investigación -->
+		<div class="container-fluid">
+			<div class="row d-flex justify-content-center fuente-institucional f-negrita texto-azul-os">
+				<div class="col-8 col-md-7 border-bottom border-top borde-azul fondo-gris-os p-2">
+					<div class="row">
+						<div class="col-12 col-sm-6 text-left text-sm-right">Coordinador:</div>
+						<div class="col-12 col-sm-6 bg-danger">Pendiente</div>
+					</div>
+				</div>
+				<div class="col-8 col-md-7 border-bottom borde-azul fondo-gris-c  p-2">
+					<div class="row">
+						<div class="col-12 col-sm-6 text-left text-sm-right">Correo electrónico:</div>
+						<div class="col-12 col-sm-6">metanola@aunar.edu.co</div>
+					</div>
+				</div>
+				<div class="col-8 col-md-7 border-bottom borde-azul fondo-gris-os p-2">
+					<div class="row">
+						<div class="col-12 col-sm-6 text-left text-sm-right">Site:</div>
+						<div class="col-12 col-sm-6 bg-danger">Pendiente</div>
+					</div>
+				</div>
+				<div class="col-8 col-md-7 border-bottom borde-azul fondo-gris-c  p-2">
+					<div class="row">
+						<div class="col-12 col-sm-6 text-left text-sm-right">Código en Colciencias:</div>
+						<div class="col-12 col-sm-6 bg-danger">Pendiente</div>
+					</div>
+				</div>
+				<div class="col-8 col-md-7 border-bottom borde-azul fondo-gris-os p-2">
+					<div class="row">
+						<div class="col-12 col-sm-6 text-left text-sm-right">Área:</div>
+						<div class="col-12 col-sm-6">Ingeniería Electrónica</div>
+					</div>
+				</div>
+				<div class="col-8 col-md-7 border-bottom borde-azul fondo-gris-c  p-2">
+					<div class="row">
+						<div class="col-12 col-sm-6 text-left text-sm-right">Colciencias:</div>
+						<div class="col-12 col-sm-6">Enlace al GrupLac</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		
+		<!-- Datos detallados de grupo de investigación -->
+		<div class="container-fluid">
+			<div class="row fuente-institucional texto-azul-os p-sm-5 d-flex justify-content-center">
+				<div class="col-9 pt-lg-3 mt-lg-4 f-myriad text-justify">
+					<p>
+						<br> <strong>Descripción:</strong><br>
+						El Grupo de investigación en Electrónica - METANOIA, es un ente académico creado para la investigación, coordinación, promoción científica y desarrollo tecnológico sin ánimo de lucro, conformado por docentes y estudiantes, cuya finalidad es investigar, colaborar, fortalecer y estimular la generación de conocimiento y desarrollo tecnológico competitivo y sostenible en el campo de electrónica, con sede en la Corporación Universitaria Autónoma de Nariño. El grupo de investigación busca mantener una participación activa, directa y permanente de sus integrantes con la comunidad universitaria, desarrollando proyectos tecnológicos competitivos, pertinentes y sostenibles en el campo de la Automatización, el Diseño Electrónico y la Telemática, dirigidos a generar desarrollo regional en un contexto nacional e internacional. Mantener una constante formación y actualización académica de todas las personas que conforman el grupo, propiciando el desarrollar de sus potencialidades en base a una cultura de investigación, trabajo en equipo y ayuda a la región.
+						<br><br> METANOIA es una palabra griega sinónimo de conversión o cambio, el cual busca sobrepasar el conocimiento de la mente he ir más allá; en contexto es trascender del pensamiento a la práctica; de esta forma METANOIA se proyecta como un grupo interdisciplinario que por sus proyectos, participaciones y producción, será reconocido por su calidad, excelencia investigativa, innovación y pertinencia de sus trabajos en el campo de la Automatización y la Telemetría aplicada, dirigida a solventar problemáticas regionales acordes a los plan de desarrollo y de competitividad del departamento.
+
+					</p>
+					<p>
+						<br> <strong>Misión</strong><br>
+						<label for="" class="bg-danger">Información no enviada</label>
+					</p>
+					<p>
+						<br> <strong>Visión</strong><br>
+						<label for="" class="bg-danger">Información no enviada</label>
+					</p>
+					<p>
+						<br> <strong>Historia </strong><br>
+						El grupo de investigación fue creado en el año de 2009 ante una necesidad de solventar necesidades del medio en el ámbito electrónico, inicialmente el grupo incursionó en el mundo de la telemetría desarrollando proyectos guiados a la tele medición de variables importantes en ambientes peligrosos y remotos como lo son las riberas de los ríos en cercanías al volcán Galeras. 
+						<br><br>Posteriormente bajo la reforma en el plan de desarrollo departamental y los avances en el plan de competitividad de Nariño bajo el gobierno del Señor Raúl Delgado en el año de 2011, el grupo decide orientar sus trabajos al fortalecimiento de cadenas productivas agrícolas y agropecuarias, buscando demostrar que la aplicación de la electrónica, la mecánica y la telemetría pueden ofrecer grandes beneficios a este tipo de procesos mejorando la calidad del producto final, minimizando riesgos de producción, cuidando el medio ambiente y lo más importante mejorando las condiciones de trabajo de los productores en busca de un mayor bienestar.
+
+					</p>
+				</div>
+			</div>
+		</div>
+
+		<!-- imágen de inmecnar -->
+		<div class="container-fluid py-5">
+			<div class="row d-flex justify-content-center">
+				<div class="col-8 col-md-8 p-0">
+					<img class="mx-auto img-fluid" width="100%" src="img/investigacion/zed-matec.png" alt="grupos de investigación zed matec AUNAR" />
+				</div>
+			</div>
+		</div>
+		
+		<!-- Datos de grupo de investigación -->
+		<div class="container-fluid">
+			<div class="row d-flex justify-content-center fuente-institucional f-negrita texto-azul-os">
+				<div class="col-8 col-md-7 border-bottom border-top borde-azul fondo-gris-os p-2">
+					<div class="row">
+						<div class="col-12 col-sm-6 text-left text-sm-right">Coordinador:</div>
+						<div class="col-12 col-sm-6 bg-danger">Constanza Eugenia Colunge Ordoñez</div>
+					</div>
+				</div>
+				<div class="col-8 col-md-7 border-bottom borde-azul fondo-gris-c  p-2">
+					<div class="row">
+						<div class="col-12 col-sm-6 text-left text-sm-right">Correo electrónico:</div>
+						<div class="col-12 col-sm-6">sedmatec@aunar.edu.co</div>
+					</div>
+				</div>
+				<div class="col-8 col-md-7 border-bottom borde-azul fondo-gris-os p-2">
+					<div class="row">
+						<div class="col-12 col-sm-6 text-left text-sm-right">Site:</div>
+						<div class="col-12 col-sm-6">https://sites.google.com/aunar.edu.co/investigacion</div>
+					</div>
+				</div>
+				<div class="col-8 col-md-7 border-bottom borde-azul fondo-gris-c  p-2">
+					<div class="row">
+						<div class="col-12 col-sm-6 text-left text-sm-right">Código en Colciencias:</div>
+						<div class="col-12 col-sm-6 bg-danger">Pendiente</div>
+					</div>
+				</div>
+				<div class="col-8 col-md-7 border-bottom borde-azul fondo-gris-os p-2">
+					<div class="row">
+						<div class="col-12 col-sm-6 text-left text-sm-right">Área:</div>
+						<div class="col-12 col-sm-6">Ingeniería Informática</div>
+					</div>
+				</div>
+				<div class="col-8 col-md-7 border-bottom borde-azul fondo-gris-c  p-2">
+					<div class="row">
+						<div class="col-12 col-sm-6 text-left text-sm-right">Colciencias:</div>
+						<div class="col-12 col-sm-6 bg-danger">Enlace al GrupLac</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		
+		<!-- Datos detallados de grupo de investigación -->
+		<div class="container-fluid">
+			<div class="row fuente-institucional texto-azul-os p-sm-5 d-flex justify-content-center">
+				<div class="col-9 pt-lg-3 mt-lg-4 f-myriad text-justify">
+					<p>
+						<br> <strong>Descripción:</strong><br>
+						<label for="" class="bg-danger">Información no enviada</label>
+
+					</p>
+					<p>
+						<br> <strong>Misión</strong><br>
+						Generar procesos de investigación en el área de ingeniería informática, en temas relacionados con el tratamiento de grandes volúmenes de datos a través de las matemáticas aplicadas y el uso de las nuevas tecnologías; que posibiliten la generación de nuevo conocimiento y aporten de manera positiva al desarrollo territorial, en sintonía con las políticas institucionales frente a la investigación.
+					</p>
+					<p>
+						<br> <strong>Visión</strong><br>
+						Para el año 2020 el grupo de investigación SEDMATEC será un grupo categorizado en el sistema nacional de investigación Colciencias, de manera que, se puedan verificar los productos de investigación en el área de la informática, favoreciendo la proyección institucional en el ámbito regional, nacional e internacional. 
+					</p>
+					<p>
+						<br> <strong>Historia </strong><br>
+						<label for="" class="bg-danger">Información no enviada</label>
+					</p>
+				</div>
+			</div>
+		</div>
+
+
 		<?php include_once('secciones/footer-principal.php');?>
 		
 	</body>
