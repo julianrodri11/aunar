3cb6d45e776255664cae4a60b70b3b97ef60c313
Julian Rodriguez <julianrodri11@gmail.com>
2018-10-02 20:15:21 -0500

    Información grupos de investigación

--- grupos-de-investigacion.php
+++ grupos-de-investigacion.php
@@ -286,6 +286,312 @@
 		</div>
 
 
+		<!-- imágen de inmecnar -->
+		<div class="container-fluid py-5">
+			<div class="row d-flex justify-content-center">
+				<div class="col-8 col-md-7 p-0">
+					<img class="mx-auto img-fluid" width="100%" src="img/investigacion/moda-+-cultura.png" alt="grupos de investigación Moda más Cultura AUNAR" />
+				</div>
+			</div>
+		</div>
+		
+		<!-- Datos de grupo de investigación -->
+		<div class="container-fluid">
+			<div class="row d-flex justify-content-center fuente-institucional f-negrita texto-azul-os">
+				<div class="col-8 col-md-7 border-bottom border-top borde-azul fondo-gris-os p-2">
+					<div class="row">
+						<div class="col-12 col-sm-6 text-left text-sm-right">Coordinador:</div>
+						<div class="col-12 col-sm-6 bg-danger">Gina Maria Hidalgo</div>
+					</div>
+				</div>
+				<div class="col-8 col-md-7 border-bottom borde-azul fondo-gris-c  p-2">
+					<div class="row">
+						<div class="col-12 col-sm-6 text-left text-sm-right">Correo electrónico:</div>
+						<div class="col-12 col-sm-6">modamascultura@gmail.com</div>
+					</div>
+				</div>
+				<div class="col-8 col-md-7 border-bottom borde-azul fondo-gris-os p-2">
+					<div class="row">
+						<div class="col-12 col-sm-6 text-left text-sm-right">Site:</div>
+						<div class="col-12 col-sm-6 bg-danger">Pendiente</div>
+					</div>
+				</div>
+				<div class="col-8 col-md-7 border-bottom borde-azul fondo-gris-c  p-2">
+					<div class="row">
+						<div class="col-12 col-sm-6 text-left text-sm-right">Código en Colciencias:</div>
+						<div class="col-12 col-sm-6 bg-danger">Pendiente</div>
+					</div>
+				</div>
+				<div class="col-8 col-md-7 border-bottom borde-azul fondo-gris-os p-2">
+					<div class="row">
+						<div class="col-12 col-sm-6 text-left text-sm-right">Área:</div>
+						<div class="col-12 col-sm-6">Diseño de Modas</div>
+					</div>
+				</div>
+				<div class="col-8 col-md-7 border-bottom borde-azul fondo-gris-c  p-2">
+					<div class="row">
+						<div class="col-12 col-sm-6 text-left text-sm-right">Colciencias:</div>
+						<div class="col-12 col-sm-6 bg-danger">Enlace al GrupLac</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		
+		<!-- Datos detallados de grupo de investigación -->
+		<div class="container-fluid">
+			<div class="row fuente-institucional texto-azul-os p-sm-5 d-flex justify-content-center">
+				<div class="col-9 pt-lg-3 mt-lg-4 f-myriad text-justify">
+					<p>
+						<br> <strong>Descripción:</strong><br>
+						Promoviendo el compromiso con la producción científica y el desarrollo de la región y el país, en el año 2012 la Universidad Autónoma de Nariño crea el Grupo de Investigación MODA + CULTURA, vinculado a la línea de Investigación “Programa Procesos Culturales” y los campos de trabajo; “Patrimonios culturales y ambientales”, “Desarrollo Empresarial” y “Observatorio de Tendencias” del programa de Diseño de Modas de la misma institución académica.
+						<br><br> La creación del grupo fue una iniciativa que tuvo la Facultad de Diseño de Modas en marzo de 2012 de promover a través de un encuentro académico el debate sobre la artesanía textil en Colombia. Nace así el principal antecedente en la creación de nuestro grupo de investigación: El 1er Encuentro de Tejido Ancestral, liderado por la Coordinadora del programa de Diseño de Moda Paula Guevara, la Antropóloga Betty Mercedes Díaz y la investigadora Diana Oliva. Este evento liderado por MODA+CULTURA se llevó a cabo del 2 al 4 de noviembre de 2012. Su objetivo fue Crear un espacio de intercomunicación entre las y los Artesanos Ancestrales específicamente los del Tejido y los Nuevos Creadores, Actores, procesos y políticas del sector. Se presentaron conferencias que analizan prácticas ancestrales en tratamiento de Fibras Naturales, Tinción y Tejeduría, siempre enmarcadas en el debate sobre la dignificación y rescate del trabajo artesanal. Se debe resaltar que la convocatoria contó con la participación del Sector Artesanal de los Pueblos Quillasingas, Pastos, Ingas, Kamsas y Afros.
+						<br><br> Ante los resultados y el aporte académico del evento, el grupo se fortaleció, ejemplo de esto fue su posterior participación en la II Rueda de Negocios de Innovación Cauca – Nariño. Actualmente se trabaja en nuevos proyectos de investigación cultural y en la realización de la segunda edición del Encuentro de Tejido Ancestral.
+					</p>
+					<p>
+						<br> <strong>Misión</strong><br>
+						<label for="" class="bg-danger">Información no enviada</label>
+					</p>
+					<p>
+						<br> <strong>Visión</strong><br>
+						El grupo MODA+CULTURA aspira a repercutir en la comunidad académica, el desarrollo sostenible y el emprendimiento empresarial del sector moda; defender el reconocimiento de los pueblos ancestrales y nuestro patrimonio natural y cultural.
+					</p>
+					<p>
+						<br> <strong>Historia </strong><br>
+						<label for="" class="bg-danger">Información no enviada</label>
+					</p>
+				</div>
+			</div>
+		</div>
+
+
+
+		<!-- imágen de inmecnar -->
+		<div class="container-fluid py-5">
+			<div class="row d-flex justify-content-center">
+				<div class="col-8 col-md-7 p-0">
+					<img class="mx-auto img-fluid" width="100%" src="img/investigacion/ciac.png" alt="grupos de investigación CIAC AUNAR" />
+				</div>
+			</div>
+		</div>
+		
+		<!-- Datos de grupo de investigación -->
+		<div class="container-fluid">
+			<div class="row d-flex justify-content-center fuente-institucional f-negrita texto-azul-os">
+				<div class="col-8 col-md-7 border-bottom border-top borde-azul fondo-gris-os p-2">
+					<div class="row">
+						<div class="col-12 col-sm-6 text-left text-sm-right">Coordinador:</div>
+						<div class="col-12 col-sm-6 bg-danger">Pendiente</div>
+					</div>
+				</div>
+				<div class="col-8 col-md-7 border-bottom borde-azul fondo-gris-c  p-2">
+					<div class="row">
+						<div class="col-12 col-sm-6 text-left text-sm-right">Correo electrónico:</div>
+						<div class="col-12 col-sm-6 bg-danger">Pendiente</div>
+					</div>
+				</div>
+				<div class="col-8 col-md-7 border-bottom borde-azul fondo-gris-os p-2">
+					<div class="row">
+						<div class="col-12 col-sm-6 text-left text-sm-right">Site:</div>
+						<div class="col-12 col-sm-6 bg-danger">Pendiente</div>
+					</div>
+				</div>
+				<div class="col-8 col-md-7 border-bottom borde-azul fondo-gris-c  p-2">
+					<div class="row">
+						<div class="col-12 col-sm-6 text-left text-sm-right">Código en Colciencias:</div>
+						<div class="col-12 col-sm-6 bg-danger">Pendiente</div>
+					</div>
+				</div>
+				<div class="col-8 col-md-7 border-bottom borde-azul fondo-gris-os p-2">
+					<div class="row">
+						<div class="col-12 col-sm-6 text-left text-sm-right">Área:</div>
+						<div class="col-12 col-sm-6">Ciencias Administrativas y Contables</div>
+					</div>
+				</div>
+				<div class="col-8 col-md-7 border-bottom borde-azul fondo-gris-c  p-2">
+					<div class="row">
+						<div class="col-12 col-sm-6 text-left text-sm-right">Colciencias:</div>
+						<div class="col-12 col-sm-6 bg-danger">Pendiente</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		
+		<!-- Datos detallados de grupo de investigación -->
+		<div class="container-fluid">
+			<div class="row fuente-institucional texto-azul-os p-sm-5 d-flex justify-content-center">
+				<div class="col-9 pt-lg-3 mt-lg-4 f-myriad text-justify">
+					<p>
+						<br> <strong>Descripción:</strong><br>
+						El equipo de investigación CIAC pertenece a la Facultad de Facultad de Ciencias Administrativas, Económicas y Contables de la Corporación Universitaria Autónoma de Nariño. Se crea para responder a las necesidades de los empresarios de la región desarrollando investigaciones disciplinares y aplicadas, consultorías, asesorías y capacitación. Además de promover, apoyar y generar la investigación en el ámbito de las ciencias administrativas y contables, desarrollando proyectos desde una perspectiva social, cultural, económica generando el desarrollo de sus capacidades y habilidades investigativas que le permitan a la universidad interactuar con el desarrollo de la región en el campo empresarial. 
+					</p>
+					<p>
+						<br> <strong>Misión</strong><br>
+						Ser un grupo reconocido en el ámbito académico universitario por el aporte de sus investigaciones, consultorías, asesorías y capacitaciones buscando el crecimiento, bienestar y desarrollo de la dinámica e innovación empresarial, social y económica de la Región. Consolidarse como un grupo dinámico, abierto a la innovación dentro del contexto educativo, con proyección social con actividades de impacto preocupado por el desarrollo de la Región. 
+					</p>
+					<p>
+						<br> <strong>Visión</strong><br>
+						<label for="" class="bg-danger">Información no enviada</label>
+					</p>
+					<p>
+						<br> <strong>Historia </strong><br>
+						<label for="" class="bg-danger">Información no enviada</label>
+					</p>
+				</div>
+			</div>
+		</div>
+
+
+		
+		<!-- imágen de inmecnar -->
+		<div class="container-fluid py-5">
+			<div class="row d-flex justify-content-center">
+				<div class="col-8 col-md-7 p-0">
+					<img class="mx-auto img-fluid" width="100%" src="img/investigacion/irop.png" alt="grupos de investigación IROP AUNAR" />
+				</div>
+			</div>
+		</div>
+		
+		<!-- Datos de grupo de investigación -->
+		<div class="container-fluid">
+			<div class="row d-flex justify-content-center fuente-institucional f-negrita texto-azul-os">
+				<div class="col-8 col-md-7 border-bottom border-top borde-azul fondo-gris-os p-2">
+					<div class="row">
+						<div class="col-12 col-sm-6 text-left text-sm-right">Coordinador:</div>
+						<div class="col-12 col-sm-6">Diana María Revelo Tovar</div>
+					</div>
+				</div>
+				<div class="col-8 col-md-7 border-bottom borde-azul fondo-gris-c  p-2">
+					<div class="row">
+						<div class="col-12 col-sm-6 text-left text-sm-right">Correo electrónico:</div>
+						<div class="col-12 col-sm-6">irop@aunar.edu.co</div>
+					</div>
+				</div>
+				<div class="col-8 col-md-7 border-bottom borde-azul fondo-gris-os p-2">
+					<div class="row">
+						<div class="col-12 col-sm-6 text-left text-sm-right">Site:</div>
+						<div class="col-12 col-sm-6 bg-danger">Pendiente</div>
+					</div>
+				</div>
+				<div class="col-8 col-md-7 border-bottom borde-azul fondo-gris-c  p-2">
+					<div class="row">
+						<div class="col-12 col-sm-6 text-left text-sm-right">Código en Colciencias:</div>
+						<div class="col-12 col-sm-6 bg-danger">Pendiente</div>
+					</div>
+				</div>
+				<div class="col-8 col-md-7 border-bottom borde-azul fondo-gris-os p-2">
+					<div class="row">
+						<div class="col-12 col-sm-6 text-left text-sm-right">Área:</div>
+						<div class="col-12 col-sm-6">Mecánica Dental</div>
+					</div>
+				</div>
+				<div class="col-8 col-md-7 border-bottom borde-azul fondo-gris-c  p-2">
+					<div class="row">
+						<div class="col-12 col-sm-6 text-left text-sm-right">Colciencias:</div>
+						<div class="col-12 col-sm-6 bg-danger">Pendiente</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		
+		<!-- Datos detallados de grupo de investigación -->
+		<div class="container-fluid">
+			<div class="row fuente-institucional texto-azul-os p-sm-5 d-flex justify-content-center">
+				<div class="col-9 pt-lg-3 mt-lg-4 f-myriad text-justify">
+					<p>
+						<br> <strong>Descripción:</strong><br>
+						<label for="" class="bg-danger">Información no enviada</label>
+					</p>
+					<p>
+						<br> <strong>Misión</strong><br>
+						<label for="" class="bg-danger">Información no enviada</label>
+					</p>
+					<p>
+						<br> <strong>Visión</strong><br>
+						En 2017 el grupo IROP aspira a ser reconocido y clasificado por COLCIENCIAS, como un grupo con calidad y articulación de los procesos de investigación, docencia, extensión y proyección social liderados por docentes-investigadores y estudiantes generando proyectos de investigación con impacto en la comunidad académica y científica con evidencia de solución de problemas y de avances en el conocimiento tecnológico en el área de mecánica dental El grupo IROP se fundamenta en una cultura investigativa basada en el esfuerzo personal y colectivo el cual se evidenciará en participación en eventos de carácter regional y nacional, publicaciones nacionales e internacionales y cursos de actualización en las líneas de investigación.
+					</p>
+					<p>
+						<br> <strong>Historia </strong><br>
+						<label for="" class="bg-danger">Información no enviada</label>
+					</p>
+				</div>
+			</div>
+		</div>
+
+
+		<!-- imágen de inmecnar -->
+		<div class="container-fluid py-5">
+			<div class="row d-flex justify-content-center">
+				<div class="col-8 col-md-7 p-0">
+					<img class="mx-auto img-fluid" width="100%" src="img/investigacion/egresados-aunar.png" alt="grupos de investigación EGRESADOS AUNAR" />
+				</div>
+			</div>
+		</div>
+		
+		<!-- Datos de grupo de investigación -->
+		<div class="container-fluid">
+			<div class="row d-flex justify-content-center fuente-institucional f-negrita texto-azul-os">
+				<div class="col-8 col-md-7 border-bottom border-top borde-azul fondo-gris-os p-2">
+					<div class="row">
+						<div class="col-12 col-sm-6 text-left text-sm-right">Coordinador:</div>
+						<div class="col-12 col-sm-6">Irina Margarita Jurado Paz</div>
+					</div>
+				</div>
+				<div class="col-8 col-md-7 border-bottom borde-azul fondo-gris-c  p-2">
+					<div class="row">
+						<div class="col-12 col-sm-6 text-left text-sm-right">Correo electrónico:</div>
+						<div class="col-12 col-sm-6">egresados@aunar.edu.co</div>
+					</div>
+				</div>
+				<div class="col-8 col-md-7 border-bottom borde-azul fondo-gris-os p-2">
+					<div class="row">
+						<div class="col-12 col-sm-6 text-left text-sm-right">Site:</div>
+						<div class="col-12 col-sm-6 bg-danger">Pendiente</div>
+					</div>
+				</div>
+				<div class="col-8 col-md-7 border-bottom borde-azul fondo-gris-c  p-2">
+					<div class="row">
+						<div class="col-12 col-sm-6 text-left text-sm-right">Código en Colciencias:</div>
+						<div class="col-12 col-sm-6 bg-danger">Pendiente</div>
+					</div>
+				</div>
+				<div class="col-8 col-md-7 border-bottom borde-azul fondo-gris-os p-2">
+					<div class="row">
+						<div class="col-12 col-sm-6 text-left text-sm-right">Área:</div>
+						<div class="col-12 col-sm-6">Egresados AUNAR</div>
+					</div>
+				</div>
+				<div class="col-8 col-md-7 border-bottom borde-azul fondo-gris-c  p-2">
+					<div class="row">
+						<div class="col-12 col-sm-6 text-left text-sm-right">Colciencias:</div>
+						<div class="col-12 col-sm-6 bg-danger">Pendiente</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		
+		<!-- Datos detallados de grupo de investigación -->
+		<div class="container-fluid">
+			<div class="row fuente-institucional texto-azul-os p-sm-5 d-flex justify-content-center">
+				<div class="col-9 pt-lg-3 mt-lg-4 f-myriad text-justify">
+					<p>
+						<br> <strong>Descripción:</strong><br>
+						<label for="" class="bg-danger">Información no enviada</label>
+					</p>
+					<p>
+						<br> <strong>Misión</strong><br>
+						<label for="" class="bg-danger">Información no enviada</label>
+					</p>
+					<p>
+						<br> <strong>Visión</strong><br>
+						En el año 2019 seremos reconocidos como un grupo de investigación pionero, dinámico y en crecimiento tanto en la institución como en la región. Para ello, nos ocuparemos por ofrecer un portafolio de servicios pertinente y de alta calidad para aportar en el desarrollo integral de los egresados de la AUNAR. 					
+					</p>
+					<p>
+						<br> <strong>Historia </strong><br>
+						<label for="" class="bg-danger">Información no enviada</label>
+					</p>
+				</div>
+			</div>
+		</div>
 		<?php include_once('secciones/footer-principal.php');?>
 		
 	</body>
