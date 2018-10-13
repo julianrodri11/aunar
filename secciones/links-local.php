8dc49beec16c029b59d321d435e575a8d8437dd9
Julian Rodriguez <julianrodri11@gmail.com>
2018-10-13 02:10:30 -0500

    NOSCRIPT PARA NAVEGADORES Y CARGA RAPIDA

--- secciones/links-local.php
+++ secciones/links-local.php
@@ -5,18 +5,18 @@
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <!-- iconos externos de awesome -->  
 <!-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"> -->
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="css/bootstrap.min.css">
 <!-- jQuery library -->
-<script type="text/javascript" src="js/jquery.min.js" async></script>
+<script async type="text/javascript" src="js/jquery.min.js"></script>
 <!-- Popper JS -->
-<script type="text/javascript" src="js/popper.min.js" async></script>
+<script async type="text/javascript" src="js/popper.min.js"></script>
+<!-- Latest compiled JavaScript -->
 
 
-<!-- CSS -->
-<!-- noscript PARA QUE GOOGLE NO LO TOME COMO SCRIPT Y CARGUE MÁS RAPIDO LA PÁGINA -->
+<!-- CSS OPTIMIZADO PARA GOOGLE -->
 <noscript id="deferred-styles">
-    <!-- Latest compiled and minified CSS -->
-    <link rel="stylesheet" href="css/bootstrap.min.css">
-    <!-- ESTILOS PERSONALES -->
+<!-- ESTILOS PERSONALES -->
     <link rel="stylesheet" type="text/css" href="css/personalizado.css">
     <!-- efectos cuando se colocal el mouse encima de algo -efectos hover -->
     <link rel="stylesheet" type="text/css" href="css/hover.css">
@@ -24,15 +24,29 @@
     <link rel="stylesheet" type="text/css" href="css/animate.css">
 </noscript>
 
-<!-- JAVASCRIPT -->
+<script>
+      var loadDeferredStyles = function() {
+        var addStylesNode = document.getElementById("deferred-styles");
+        var replacement = document.createElement("div");
+        replacement.innerHTML = addStylesNode.textContent;
+        document.body.appendChild(replacement)
+        addStylesNode.parentElement.removeChild(addStylesNode);
+      };
+      var raf = window.requestAnimationFrame || window.mozRequestAnimationFrame ||
+          window.webkitRequestAnimationFrame || window.msRequestAnimationFrame;
+      if (raf) raf(function() { window.setTimeout(loadDeferredStyles, 0); });
+      else window.addEventListener('load', loadDeferredStyles);
+</script>
+<!-- FIN CSS OPTIMIZADO PARA GOOGLE -->
+
+
 <!-- framework para maquetación  -->
-<!-- Latest compiled JavaScript -->
-<script type="text/javascript" src="js/bootstrap.min.js" async></script>
+<script async type="text/javascript" src="js/bootstrap.min.js"></script>
 <!-- para activar complementos de bootstrar o hacer otras acciones  -->
-<script type="text/javascript" src="js/personalizado.js" async></script>
+<script async type="text/javascript" src="js/personalizado.js"></script>
 <!-- efectos cuando se baje el scroll -->
-<script type="text/javascript" src="js/jquery.smoove.min.js" async></script>
+<script async type="text/javascript" src="js/jquery.smoove.min.js"></script>
 <!-- efectos personalizados -->
-<script type="text/javascript" src="js/efectos_smoove.js" async></script>
+<script async type="text/javascript" src="js/efectos_smoove.js"></script>
 
 <?php /*VARIABLE QUE SACA EL NOMBRE DEL SERVIDOR*/ $servidor = $_SERVER['SERVER_NAME']; ?>
