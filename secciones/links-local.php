b69dd512c127cf1436f38e7f1d33ab45b9a33217
Julian Rodriguez <julianrodri11@gmail.com>
2018-10-13 02:19:45 -0500

    PRUEBAS

--- secciones/links-local.php
+++ secciones/links-local.php
@@ -5,48 +5,28 @@
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <!-- iconos externos de awesome -->  
 <!-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"> -->
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
 <!-- jQuery library -->
-<script async type="text/javascript" src="js/jquery.min.js"></script>
+<script  type="text/javascript" src="js/jquery.min.js" async></script>
 <!-- Popper JS -->
-<script async type="text/javascript" src="js/popper.min.js"></script>
+<script  type="text/javascript" src="js/popper.min.js" async></script>
 <!-- Latest compiled JavaScript -->
-
-
 <!-- CSS OPTIMIZADO PARA GOOGLE -->
-<noscript id="deferred-styles">
-    <!-- Latest compiled and minified CSS -->
-    <link rel="stylesheet" href="css/bootstrap.min.css">
-    <!-- ESTILOS PERSONALES -->
-    <link rel="stylesheet" type="text/css" href="css/personalizado.css">
-    <!-- efectos cuando se colocal el mouse encima de algo -efectos hover -->
-    <link rel="stylesheet" type="text/css" href="css/hover.css">
-    <!-- libreria para animaciones del hover de una imágen -->  
-    <link rel="stylesheet" type="text/css" href="css/animate.css">
-</noscript>
-
-<script>
-      var loadDeferredStyles = function() {
-        var addStylesNode = document.getElementById("deferred-styles");
-        var replacement = document.createElement("div");
-        replacement.innerHTML = addStylesNode.textContent;
-        document.body.appendChild(replacement)
-        addStylesNode.parentElement.removeChild(addStylesNode);
-      };
-      var raf = window.requestAnimationFrame || window.mozRequestAnimationFrame ||
-          window.webkitRequestAnimationFrame || window.msRequestAnimationFrame;
-      if (raf) raf(function() { window.setTimeout(loadDeferredStyles, 0); });
-      else window.addEventListener('load', loadDeferredStyles);
-</script>
+<!-- ESTILOS PERSONALES -->
+<link rel="stylesheet" type="text/css" href="css/personalizado.css">
+<!-- efectos cuando se colocal el mouse encima de algo -efectos hover -->
+<link rel="stylesheet" type="text/css" href="css/hover.css">
+<!-- libreria para animaciones del hover de una imágen -->  
+<link rel="stylesheet" type="text/css" href="css/animate.css">
 <!-- FIN CSS OPTIMIZADO PARA GOOGLE -->
-
-
 <!-- framework para maquetación  -->
-<script async type="text/javascript" src="js/bootstrap.min.js"></script>
+<script  type="text/javascript" src="js/bootstrap.min.js" async></script>
 <!-- para activar complementos de bootstrar o hacer otras acciones  -->
-<script async type="text/javascript" src="js/personalizado.js"></script>
+<script  type="text/javascript" src="js/personalizado.js" async></script>
 <!-- efectos cuando se baje el scroll -->
-<script async type="text/javascript" src="js/jquery.smoove.min.js"></script>
+<script  type="text/javascript" src="js/jquery.smoove.min.js" async></script>
 <!-- efectos personalizados -->
-<script async type="text/javascript" src="js/efectos_smoove.js"></script>
+<script  type="text/javascript" src="js/efectos_smoove.js" async></script>
 
 <?php /*VARIABLE QUE SACA EL NOMBRE DEL SERVIDOR*/ $servidor = $_SERVER['SERVER_NAME']; ?>
