5ca78e28cc21df302da6193a90c41f2daf671c91
Julian Rodriguez <julianrodri11@gmail.com>
2018-10-20 12:26:20 -0500

    quita los comentarios para w3c

--- secciones/modal-traductor.php
+++ secciones/modal-traductor.php
@@ -1,45 +1,26 @@
-<!DOCTYPE html>
-<html lang="es">
-<head>
-	<meta charset="UTF-8">
-	<title>Traducir página de AUNAR</title>
-
-</head>
-<body>
-	
-
-
 <div class="container-fluid">
-<!-- The Modal -->
+  <!-- The Modal -->
   <div class="modal fade " id="btn_traductor">
     <div class="modal-dialog modal-dialog-centered ">
-      <div class="modal-content">
-      
+      <div class="modal-content">        
         <!-- Modal Header -->
         <div class="modal-header">
           <h4 class="modal-title fuente-institucional texto-azul">Traducir</h4>
           <button type="button" class="close" data-dismiss="modal">&times;</button>
-        </div>
-        
+        </div>        
         <!-- Modal body -->
-        <div class="modal-body text-center">
-        
-        <!-- -------------------------TRADUCTOR-------------------------- -->
-        <!-- este script es generado automaticamente con google-->
-		<div id="google_translate_element" ></div>
-
-			<script type="text/javascript">
-				function googleTranslateElementInit() {
-				  new google.translate.TranslateElement({pageLanguage: 'es'}, 'google_translate_element');
-				}
-			</script>
-
-			<script type="text/javascript" src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>
-				
-
-		<!-- ---------------------FIN TRADUCTOR-------------------------- -->
-        </div>
-        
+        <div class="modal-body text-center">          
+          <!-- -------------------------TRADUCTOR-------------------------- -->
+          <!-- este script es generado automaticamente con google-->
+          <div id="google_translate_element" ></div>
+          <script type="text/javascript">
+            function googleTranslateElementInit() {
+            new google.translate.TranslateElement({pageLanguage: 'es'}, 'google_translate_element');
+            }
+          </script>
+          <script type="text/javascript" src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>          
+          <!-- ---------------------FIN TRADUCTOR-------------------------- -->
+        </div>        
         <!-- Modal footer -->
         <div class="modal-footer">
           <button type="button" class="btn btn-secondary fondo-azul-oscuro fuente-institucional" data-dismiss="modal">Cerrar</button>
@@ -48,7 +29,4 @@
       </div>
     </div>
   </div>
-
-  </div>
-</body>
-</html>
+</div>
