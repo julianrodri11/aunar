2f48380635a8ac35ab06c5d938e263daca915e28
Julian Rodriguez <julianrodri11@gmail.com>
2018-10-22 01:10:03 -0500

    Cambio ubaciòn bootstrap

--- secciones/links-local.php
+++ secciones/links-local.php
@@ -22,9 +22,9 @@ window.webkitRequestAnimationFrame || window.msRequestAnimationFrame;
 if (raf) raf(function() { window.setTimeout(loadDeferredStyles, 0); });
 else window.addEventListener('load', loadDeferredStyles);
 </script>
+<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
 <script  type="text/javascript" src="js/bootstrap.min.js" async></script>
 <script  type="text/javascript" src="js/personalizado.js" async></script>
 <script  type="text/javascript" src="js/jquery.smoove.min.js" async></script>
 <script  type="text/javascript" src="js/efectos_smoove.js" async></script>
-<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
 <?php /*VARIABLE QUE SACA EL NOMBRE DEL SERVIDOR*/ $servidor = $_SERVER['SERVER_NAME']; ?>
