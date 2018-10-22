0a6f7b2d0f9da46d370ec0e0b269751499c6126c
Julian Rodriguez <julianrodri11@gmail.com>
2018-10-22 02:19:02 -0500

    cambios

--- secciones/links-local.php
+++ secciones/links-local.php
@@ -1,6 +1,9 @@
 <meta charset="UTF-8">
 <link rel="shortcut icon" href="img/aunar.ico">
 <meta name="viewport" content="width=device-width, initial-scale=1">
+<script  type="text/javascript" src="js/jquery.min.js" async></script>
+<script  type="text/javascript" src="js/popper.min.js" async></script>
+<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
 <noscript id="deferred-styles">
 <link rel="stylesheet" type="text/css" href="css/personalizado.css">
 <link rel="stylesheet" type="text/css" href="css/hover.css">
@@ -19,9 +22,6 @@ window.webkitRequestAnimationFrame || window.msRequestAnimationFrame;
 if (raf) raf(function() { window.setTimeout(loadDeferredStyles, 0); });
 else window.addEventListener('load', loadDeferredStyles);
 </script>
-<script  type="text/javascript" src="js/jquery.min.js" async></script>
-<script  type="text/javascript" src="js/popper.min.js" async></script>
-<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
 <script  type="text/javascript" src="js/bootstrap.min.js" async></script>
 <script  type="text/javascript" src="js/personalizado.js" async></script>
 <script  type="text/javascript" src="js/jquery.smoove.min.js" async></script>
