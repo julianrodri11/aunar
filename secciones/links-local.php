87f43e21af8d5db46d842c71ebf6844965c18f66
Julian Rodriguez <julianrodri11@gmail.com>
2018-10-22 02:15:20 -0500

    cambio de boots

--- secciones/links-local.php
+++ secciones/links-local.php
@@ -1,8 +1,6 @@
 <meta charset="UTF-8">
 <link rel="shortcut icon" href="img/aunar.ico">
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<script  type="text/javascript" src="js/jquery.min.js" async></script>
-<script  type="text/javascript" src="js/popper.min.js" async></script>
 <noscript id="deferred-styles">
 <link rel="stylesheet" type="text/css" href="css/personalizado.css">
 <link rel="stylesheet" type="text/css" href="css/hover.css">
@@ -21,6 +19,8 @@ window.webkitRequestAnimationFrame || window.msRequestAnimationFrame;
 if (raf) raf(function() { window.setTimeout(loadDeferredStyles, 0); });
 else window.addEventListener('load', loadDeferredStyles);
 </script>
+<script  type="text/javascript" src="js/jquery.min.js" async></script>
+<script  type="text/javascript" src="js/popper.min.js" async></script>
 <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
 <script  type="text/javascript" src="js/bootstrap.min.js" async></script>
 <script  type="text/javascript" src="js/personalizado.js" async></script>
